4a266619cff3c85d6b089bfe8fb3db9d81eabd98
TobiasPrie <49028782+TobiasPrie@users.noreply.github.com>
2020-02-28 20:37:29 +0100

    Update BernsteinVazirani.hpp

--- include/algorithms/BernsteinVazirani.hpp
+++ include/algorithms/BernsteinVazirani.hpp
@@ -10,21 +10,18 @@ namespace qc {
 
 		void oracle(QuantumComputation& qc);
 
-		void diffusion(QuantumComputation& qc);
+		void postProcessing(QuantumComputation& qc);
 
 		void full_BernsteinVazirani(QuantumComputation& qc);
 
 	public:
 		unsigned int       hiddenInteger = 0;
 		unsigned int	   size = -1;
-		unsigned long long iterations = 1;
 		bool               includeSetup = true;
 
 		explicit BernsteinVazirani(unsigned int hiddenInt);
 
 		~BernsteinVazirani() override = default;
-
-		dd::Edge buildFunctionality(std::unique_ptr<dd::Package>& dd) override;
 
 		dd::Edge simulate(const dd::Edge& in, std::unique_ptr<dd::Package>& dd) override;
 
