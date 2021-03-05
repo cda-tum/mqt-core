483a431e768afb5de7907098a40a9270e49f6d87
Lukas Burgholzer <lukas.burgholzer@jku.at>
2021-03-05 18:12:30 +0100

    Move functionality for handling ancillary and garbage qubits to DD package
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- include/QuantumComputation.hpp
+++ include/QuantumComputation.hpp
@@ -140,13 +140,14 @@ namespace qc {
 		[[nodiscard]] bool physicalQubitIsAncillary(unsigned short physical_qubit_index) const;
 		[[nodiscard]] bool logicalQubitIsAncillary(unsigned short logical_qubit_index) const { return ancillary.test(logical_qubit_index); }
 		void setLogicalQubitAncillary(unsigned short logical_qubit_index) { ancillary.set(logical_qubit_index); }
-		dd::Edge reduceAncillae(dd::Edge& e, std::unique_ptr<dd::Package>& dd, bool regular = true) const;
-		dd::Edge reduceAncillaeRecursion(dd::Edge& e, std::unique_ptr<dd::Package>& dd, unsigned short lowerbound, bool regular = true) const;
+		dd::Edge reduceAncillae(dd::Edge& e, std::unique_ptr<dd::Package>& dd, bool regular = true) const {
+			return dd->reduceAncillae(e, ancillary, regular);
+		}
 		[[nodiscard]] bool logicalQubitIsGarbage(unsigned short logical_qubit_index) const { return garbage.test(logical_qubit_index); }
 		void setLogicalQubitGarbage(unsigned short logical_qubit_index) { garbage.set(logical_qubit_index); }
-		// works for reversible circuits --- to be tested for quantum circuits
-		dd::Edge reduceGarbage(dd::Edge& e, std::unique_ptr<dd::Package>& dd, bool regular = true) const;
-		dd::Edge reduceGarbageRecursion(dd::Edge& e, std::unique_ptr<dd::Package>& dd, unsigned short lowerbound, bool regular = true) const;
+		dd::Edge reduceGarbage(dd::Edge& e, std::unique_ptr<dd::Package>& dd, bool regular = true) const {
+			return dd->reduceGarbage(e, garbage, regular);
+		}
 		dd::Edge createInitialMatrix(std::unique_ptr<dd::Package>& dd) const; // creates identity matrix, which is reduced with respect to the ancillary qubits
 
 		/// strip away qubits with no operations applied to them and which do not pop up in the output permutation
