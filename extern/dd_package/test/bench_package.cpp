b1b0eb899719bdae7633f0a0dd9dfb61842b3900
Lukas Burgholzer <lukas.burgholzer@jku.at>
2021-03-22 23:46:56 +0100

    ✅ Add new benchmark and remove small instances

--- extern/dd_package/test/bench_package.cpp
+++ extern/dd_package/test/bench_package.cpp
@@ -8,13 +8,32 @@
 #include <benchmark/benchmark.h>
 #include <memory>
 
+///
+/// Test DD Package creation
+///
+/// At the moment packages are allocated with a fixed maximum number of qubits (=128)
+/// In the future, the maximum number of qubits can be set at construction time
+/// until then, each creation should take equally long
+///
+
 static void BM_PackageCreation(benchmark::State& state) {
     for (auto _: state) {
-        // at the moment packages are allocated with a fixed maximum number of qubits (=128)
-        // in the future, the maximum number of qubits can be set at construction time
-        // until then, each creation takes equally long
         [[maybe_unused]] unsigned short nqubits = state.range(0);
         auto                            dd      = std::make_unique<dd::Package>();
     }
 }
-BENCHMARK(BM_PackageCreation)->Unit(benchmark::kMillisecond)->RangeMultiplier(2)->Range(1, 128);
+BENCHMARK(BM_PackageCreation)->Unit(benchmark::kMillisecond)->RangeMultiplier(2)->Range(16, 128);
+
+///
+/// Test creation of identity matrix
+///
+
+static void BM_MakeIdent(benchmark::State& state) {
+    [[maybe_unused]] unsigned short nqubits = state.range(0);
+    auto                            dd      = std::make_unique<dd::Package>();
+    for (auto _: state) {
+        dd->reset();
+        benchmark::DoNotOptimize(dd->makeIdent(nqubits));
+    }
+}
+BENCHMARK(BM_MakeIdent)->Unit(benchmark::kMillisecond)->RangeMultiplier(2)->Range(16, 128);
