2d08fcb6b1f8da092097c6b28412e35c2184299e
Lukas Burgholzer <lukas.burgholzer@jku.at>
2021-04-27 15:22:29 +0200

    ⚡ consider as many significant digits for `ComplexValue` hash as given by the tolerance
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- extern/dd_package/include/dd/ComplexValue.hpp
+++ extern/dd_package/include/dd/ComplexValue.hpp
@@ -220,8 +220,8 @@ namespace std {
     template<>
     struct hash<dd::ComplexValue> {
         std::size_t operator()(dd::ComplexValue const& c) const noexcept {
-            auto h1 = dd::murmur64(static_cast<std::size_t>(std::round(c.r * 1000)));
-            auto h2 = dd::murmur64(static_cast<std::size_t>(std::round(c.i * 1000)));
+            auto h1 = dd::murmur64(static_cast<std::size_t>(std::round(c.r / dd::ComplexTable<>::tolerance())));
+            auto h2 = dd::murmur64(static_cast<std::size_t>(std::round(c.i / dd::ComplexTable<>::tolerance())));
             return dd::combineHash(h1, h2);
         }
     };
