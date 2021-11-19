efae110e3a3829b3c28b91cb7c0d718d5c3ecca2
Lukas Burgholzer <lukas.burgholzer@jku.at>
2021-11-19 20:26:13 +0100

    ✅ increase number of qubits fot test
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- test/algorithms/eval_dynamic_circuits.cpp
+++ test/algorithms/eval_dynamic_circuits.cpp
@@ -424,7 +424,7 @@ protected:
 };
 
 INSTANTIATE_TEST_SUITE_P(Eval, DynamicCircuitEvalBV,
-                         testing::Range<std::size_t>(1U, 65U),
+                         testing::Range<std::size_t>(1U, 64U),
                          [](const testing::TestParamInfo<DynamicCircuitEvalExactQPE::ParamType>& info) {
                              dd::QubitCount nqubits = info.param;
                              std::stringstream ss{};
