6dbeb4183bee5163843934d8f7954f9000be8f42
Thomas Grurl <thomas.grurl@fh-hagenberg.at>
2022-01-11 13:17:18 +0100

    update bindings for new ecc

--- mqt/qfr/bindings.cpp
+++ mqt/qfr/bindings.cpp
@@ -17,6 +17,7 @@
 #include <eccs/Q7SteaneEcc.hpp>
 #include <eccs/Q9SurfaceEcc.hpp>
 #include <eccs/Q9ShorEcc.hpp>
+#include <eccs/Q18SurfaceEcc.hpp>
 
 #include <chrono>
 #include <pybind11/complex.h>
@@ -199,7 +200,9 @@ py::dict apply_ecc(const py::object &circ, const std::string &eccString, const i
         mapper = new Q9ShorEcc(qc, measureFrequency, decomposeMC, cliffOnly);
     } else if (eccName.compare(Q9SurfaceEcc::getName()) == 0) {
         mapper = new Q9SurfaceEcc(qc, measureFrequency, decomposeMC, cliffOnly);
-    } else {
+    } else if (eccName.compare(Q18SurfaceEcc::getName()) == 0) {
+        mapper = new Q18SurfaceEcc(qc, measureFrequency, decomposeMC, cliffOnly);
+    }else {
         std::stringstream ss{};
         ss << "No ECC found for " << eccName << " ";
         ss << "Available ECCs: ";
@@ -207,7 +210,8 @@ py::dict apply_ecc(const py::object &circ, const std::string &eccString, const i
         ss << Q3ShorEcc::getName() << ", ";
         ss << Q7SteaneEcc::getName() << ", ";
         ss << Q9ShorEcc::getName() << ", ";
-        ss << Q9SurfaceEcc::getName();
+        ss << Q9SurfaceEcc::getName() << ", ";
+        ss << Q18SurfaceEcc::getName();
         return py::dict("error"_a = ss.str());
     }
     mapper->apply();
