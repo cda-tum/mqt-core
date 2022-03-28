a7035a4062a5ebd555ac67f49f420fbf6a118c30
burgholzer <burgholzer@me.com>
2022-03-28 16:32:14 +0200

    🎨 fix file header links
    Signed-off-by: burgholzer <burgholzer@me.com>

--- include/algorithms/BernsteinVazirani.hpp
+++ include/algorithms/BernsteinVazirani.hpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of MQT QFR library which is released under the MIT license.
- * See file README.md or go to http://iic.jku.at/eda/research/quantum/ for more information.
+ * See file README.md or go to https://www.cda.cit.tum.de/research/quantum/ for more information.
  */
 
 #ifndef QFR_BV_H
