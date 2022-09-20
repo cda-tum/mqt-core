c13267c23287b44b81fdfc43e5ff9c8702193d4b
pichristoph <pichristoph@hotmail.com>
2022-09-20 14:28:03 +0200

    idle ecc: copy all gates instead of manually writing one by one

--- src/eccs/IdEcc.cpp
+++ src/eccs/IdEcc.cpp
@@ -7,6 +7,7 @@
 
 IdEcc::IdEcc(qc::QuantumComputation& qc, int measureFq, bool decomposeMC, bool cliffOnly):
     Ecc({ID::Id, 1, 0, IdEcc::getName()}, qc, measureFq, decomposeMC, cliffOnly) {
+    this->qcMapped = qc.clone();
 }
 
 void IdEcc::writeEncoding() {}
@@ -16,42 +17,23 @@ void IdEcc::measureAndCorrect() {}
 void IdEcc::writeDecoding() {}
 
 void IdEcc::mapGate(const std::unique_ptr<qc::Operation>& gate, qc::QuantumComputation& qc) {
-    const int nQubits = qc.getNqubits();
-    switch (gate.get()->getType()) {
-        case qc::I: break;
-        case qc::X:
-        case qc::H:
-        case qc::Y:
-        case qc::Z:
-        case qc::S:
-        case qc::Sdag:
-        case qc::T:
-        case qc::Tdag:
-        case qc::V:
-        case qc::Vdag:
-            for (const auto& target: gate->getTargets()) {
-                if (!gate->getControls().empty()) {
-                    qcMapped.emplace_back<qc::StandardOperation>(nQubits * ecc.nRedundantQubits, gate->getControls(), target, gate->getType());
-                } else {
-                    qcMapped.emplace_back<qc::StandardOperation>(nQubits * ecc.nRedundantQubits, target, gate->getType());
-                }
-            }
-            break;
-        case qc::U3:
-        case qc::U2:
-        case qc::Phase:
-        case qc::SX:
-        case qc::SXdag:
-        case qc::RX:
-        case qc::RY:
-        case qc::RZ:
-        case qc::SWAP:
-        case qc::iSWAP:
-        case qc::Peres:
-        case qc::Peresdag:
-        case qc::Compound:
-        case qc::ClassicControlled:
-        default:
-            throw qc::QFRException("Gate not possible to encode in error code!");
+    //gates have already been written to 'qcMapped' in the constructor
+    if (gate->getNcontrols() > 1 && decomposeMultiControlledGates) {
+        gateNotAvailableError(gate);
+    }
+    if (cliffordGatesOnly) {
+        switch (gate->getType()) {
+            case qc::H:
+            case qc::X:
+            case qc::Y:
+            case qc::Z:
+            case qc::I:
+            case qc::S:
+            case qc::T:
+                break;
+            default:
+                gateNotAvailableError(gate);
+                break;
+        }
     }
 }
