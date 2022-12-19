944591554c840e0434bf427b8cf892256566b5eb
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-12-19 13:40:11 +0100

    🔧 enable IPO by default
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- extern/dd_package/cmake/StandardProjectSettings.cmake
+++ extern/dd_package/cmake/StandardProjectSettings.cmake
@@ -19,7 +19,7 @@ set_property(GLOBAL PROPERTY CXX_EXTENSIONS OFF)
 # Generate compile_commands.json to make it easier to work with clang based tools
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-option(ENABLE_IPO "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)" OFF)
+option(ENABLE_IPO "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)" ON)
 
 if(ENABLE_IPO)
   include(CheckIPOSupported)
