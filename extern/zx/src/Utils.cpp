6fa272578e1bdec2a9b98c1f4046127040dbb792
Lukas Burgholzer <lukas.burgholzer@jku.at>
2023-06-04 11:19:49 +0200

    🚨 resolve some further compiler warnings

--- extern/zx/src/Utils.cpp
+++ extern/zx/src/Utils.cpp
@@ -4,11 +4,11 @@
 
 namespace zx {
 Vertices::VertexIterator::VertexIterator(
-    const std::vector<std::optional<VertexData>>& vertices, const Vertex v)
-    : v(v), currentPos(vertices.begin()), vertices(vertices) {
+    const std::vector<std::optional<VertexData>>& verts, const Vertex vertex)
+    : v(vertex), currentPos(verts.begin()), vertices(verts) {
   if (v >= vertices.size()) {
     currentPos = vertices.end();
-    this->v = vertices.size();
+    v = vertices.size();
   } else {
     currentPos = vertices.begin() + static_cast<int>(v);
     nextValidVertex();
@@ -48,10 +48,10 @@ void Vertices::VertexIterator::nextValidVertex() {
 }
 
 Edges::EdgeIterator::EdgeIterator(
-    const std::vector<std::vector<Edge>>& edges,
-    const std::vector<std::optional<VertexData>>& vertices)
-    : v(0), currentPos(edges[0].begin()), edgesPos(edges.begin()), edges(edges),
-      vertices(vertices) {
+    const std::vector<std::vector<Edge>>& es,
+    const std::vector<std::optional<VertexData>>& verts)
+    : v(0), currentPos(es[0].begin()), edgesPos(es.begin()), edges(es),
+      vertices(verts) {
   if (!vertices.empty()) {
     while (v < edges.size() && !vertices[v].has_value()) {
       ++v;
@@ -67,9 +67,9 @@ Edges::EdgeIterator::EdgeIterator(
 }
 
 Edges::EdgeIterator::EdgeIterator(
-    const std::vector<std::vector<Edge>>& edges,
-    const std::vector<std::optional<VertexData>>& vertices, const Vertex v)
-    : v(v), edges(edges), vertices(vertices) {
+    const std::vector<std::vector<Edge>>& es,
+    const std::vector<std::optional<VertexData>>& verts, const Vertex vertex)
+    : v(vertex), edges(es), vertices(verts) {
   if (v >= edges.size()) {
     currentPos = edges.back().end();
     edgesPos = edges.end();
