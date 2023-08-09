7b586d56b3c8f4352ea006e1ec64c5bfe2d2d534
Tianyi Wang <56802240+tyi1025@users.noreply.github.com>
2023-08-09 11:10:32 +0200

    Apply suggestions from code review
    Co-authored-by: Lukas Burgholzer <burgholzer@me.com>

--- test/algorithms/test_wstate.cpp
+++ test/algorithms/test_wstate.cpp
@@ -7,27 +7,15 @@
 #include <vector>
 
 class WState : public testing::TestWithParam<std::size_t> {
-protected:
-  void TearDown() override {}
-  void SetUp() override {}
 };
 
-std::vector<std::string> generateWStateStrings(std::size_t length) {
+std::vector<std::string> generateWStateStrings(const std::size_t length) {
   std::vector<std::string> result;
-  for (int i = 0; i < (1 << length); i++) {
-    int countOnes = 0;
-    std::string binaryString;
-    for (size_t j = 0; j < length; j++) {
-      if (((i >> j) & 1) != 0) {
-        countOnes++;
-        binaryString += "1";
-      } else {
-        binaryString += "0";
-      }
-    }
-    if (countOnes == 1) {
-      result.push_back(binaryString);
-    }
+  result.reserve(length);
+  for (std::size_t i = 0U; i < length; ++i) {
+    auto binaryString = std::string('0', length);
+    binaryString[i] = '1';
+    result.emplace_back(binaryString);
   }
   return result;
 }
@@ -50,9 +38,7 @@ TEST_P(WState, FunctionTest) {
   const std::size_t shots = 1024;
   auto measurements =
       simulate(&qc, dd->makeZeroState(qc.getNqubits()), dd, shots);
-  auto results = generateWStateStrings(nq);
-
-  for (const auto& result : results) {
+  for (const auto& result : generateWStateStrings(nq)) {
     EXPECT_TRUE(measurements.find(result) != measurements.end());
   }
 }
