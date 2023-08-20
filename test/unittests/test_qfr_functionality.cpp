e39baa8df1d3831cb738d1959c187c93a83593cd
burgholzer <burgholzer@me.com>
2023-08-20 23:22:17 +0200

    🧪 add test that fails
    Signed-off-by: burgholzer <burgholzer@me.com>

--- test/unittests/test_qfr_functionality.cpp
+++ test/unittests/test_qfr_functionality.cpp
@@ -652,6 +652,20 @@ TEST_F(QFRFunctionality, removeFinalMeasurementsWithOperationsInFront) {
   ASSERT_EQ(qc.getNindividualOps(), 6);
 }
 
+TEST_F(QFRFunctionality, removeFinalMeasurementsWithBarrier) {
+  const std::size_t nqubits = 2;
+  QuantumComputation qc(nqubits);
+  qc.barrier({0, 1});
+  qc.measure(0, 0);
+  qc.measure(1, 1);
+  std::cout << "-----------------------------\n";
+  qc.print(std::cout);
+  CircuitOptimizer::removeFinalMeasurements(qc);
+  std::cout << "-----------------------------\n";
+  qc.print(std::cout);
+  EXPECT_TRUE(qc.empty());
+}
+
 TEST_F(QFRFunctionality, gateShortCutsAndCloning) {
   // This test checks if the gate shortcuts are working correctly
   // and if the cloning of gates is working correctly.
