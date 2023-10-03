0545a20d6fa852f9196f07dfe73db9bd74923a11
burgholzer <burgholzer@me.com>
2023-10-03 20:21:13 +0200

    ♻️ improved Control bindings
    Signed-off-by: burgholzer <burgholzer@me.com>

--- include/operations/Control.hpp
+++ include/operations/Control.hpp
@@ -3,6 +3,7 @@
 #include "Definitions.hpp"
 
 #include <set>
+#include <sstream>
 
 namespace qc {
 struct Control {
@@ -10,6 +11,18 @@ struct Control {
 
   Qubit qubit{};
   Type type = Type::Pos;
+
+  [[nodiscard]] std::string toString() const {
+    std::ostringstream oss{};
+    oss << "Control(qubit=" << qubit << ", type_=\"";
+    if (type == Type::Pos) {
+      oss << "Pos";
+    } else {
+      oss << "Neg";
+    }
+    oss << "\")";
+    return oss.str();
+  }
 };
 
 inline bool operator<(const Control& lhs, const Control& rhs) {
--- src/python/operations/register_control.cpp
+++ src/python/operations/register_control.cpp
@@ -6,21 +6,32 @@ namespace mqt {
 void registerControl(py::module& m) {
 
   auto control = py::class_<qc::Control>(m, "Control");
-  control
-      .def(py::init<qc::Qubit, qc::Control::Type>(), "qubit"_a,
-           "type_"_a = qc::Control::Type::Pos,
-           "Create a control qubit of the specified control type.")
-      .def_readwrite(
-          "type_", &qc::Control::type,
-          "The type of the control qubit. Can be positive or negative.")
-      .def_readwrite("qubit", &qc::Control::qubit,
-                     "The qubit index of the control qubit.");
-
-  py::enum_<qc::Control::Type>(control, "Type")
-      .value("Pos", qc::Control::Type::Pos)
-      .value("Neg", qc::Control::Type::Neg)
-      .export_values();
+  auto controlType = py::enum_<qc::Control::Type>(control, "Type");
+  controlType.value("Pos", qc::Control::Type::Pos);
+  controlType.value("Neg", qc::Control::Type::Neg);
+  controlType.export_values();
+  controlType.def("__str__", [](const qc::Control::Type& type) {
+    return type == qc::Control::Type::Pos ? "Pos" : "Neg";
+  });
+  controlType.def("__repr__", [](const qc::Control::Type& type) {
+    return type == qc::Control::Type::Pos ? "Pos" : "Neg";
+  });
+  controlType.def("__bool__", [](const qc::Control::Type& type) {
+    return type == qc::Control::Type::Pos;
+  });
+  py::implicitly_convertible<py::bool_, qc::Control::Type>();
   py::implicitly_convertible<py::str, qc::Control::Type>();
+
+  control.def(py::init<qc::Qubit, qc::Control::Type>(), "qubit"_a,
+              "type_"_a = qc::Control::Type::Pos,
+              "Create a control qubit of the specified control type.");
+  control.def_readwrite(
+      "type_", &qc::Control::type,
+      "The type of the control qubit. Can be positive or negative.");
+  control.def_readwrite("qubit", &qc::Control::qubit,
+                        "The qubit index of the control qubit.");
+  control.def("__str__", [](const qc::Control& c) { return c.toString(); });
+  control.def("__repr__", [](const qc::Control& c) { return c.toString(); });
 }
 
 } // namespace mqt
