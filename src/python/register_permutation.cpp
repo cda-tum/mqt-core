4a27986c68e7d13748fe6c37fc218907ee9801aa
burgholzer <burgholzer@me.com>
2023-10-11 22:47:19 +0200

    🚸 better bindings for the Permutation class
    Signed-off-by: burgholzer <burgholzer@me.com>

--- src/python/register_permutation.cpp
+++ src/python/register_permutation.cpp
@@ -8,7 +8,7 @@ void registerPermutation(py::module& m) {
                               "Class representing a permutation of qubits.")
       .def(py::init([](const py::dict& p) {
              qc::Permutation perm;
-             for (auto& [key, value] : p) {
+             for (const auto& [key, value] : p) {
                perm[key.cast<qc::Qubit>()] = value.cast<qc::Qubit>();
              }
              return perm;
@@ -17,23 +17,47 @@ void registerPermutation(py::module& m) {
       .def("apply",
            py::overload_cast<const qc::Controls&>(&qc::Permutation::apply,
                                                   py::const_),
+           "controls"_a,
            "Apply the permutation to a set of controls and return the permuted "
            "controls.")
       .def("apply",
            py::overload_cast<const qc::Targets&>(&qc::Permutation::apply,
                                                  py::const_),
+           "targets"_a,
            "Apply the permutation to a set of targets and return the permuted "
            "targets.")
       .def("__getitem__",
            [](const qc::Permutation& p, const qc::Qubit q) { return p.at(q); })
       .def("__setitem__", [](qc::Permutation& p, const qc::Qubit q,
                              const qc::Qubit r) { p.at(q) = r; })
+      .def("__delitem__",
+           [](qc::Permutation& p, const qc::Qubit q) { p.erase(q); })
+      .def("__len__", &qc::Permutation::size)
       .def(
           "__iter__",
           [](const qc::Permutation& p) {
             return py::make_iterator(p.begin(), p.end());
           },
-          py::keep_alive<0, 1>());
+          py::keep_alive<0, 1>())
+      .def("__str__",
+           [](const qc::Permutation& p) {
+             std::stringstream ss;
+             ss << "{";
+             for (const auto& [k, v] : p) {
+               ss << k << ": " << v << ", ";
+             }
+             ss << "}";
+             return ss.str();
+           })
+      .def("__repr__", [](const qc::Permutation& p) {
+        std::stringstream ss;
+        ss << "Permutation({";
+        for (const auto& [k, v] : p) {
+          ss << k << ": " << v << ", ";
+        }
+        ss << "})";
+        return ss.str();
+      });
 }
 
 } // namespace mqt
