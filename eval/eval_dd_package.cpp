63c1f581f6a7733a278df4b0ac63dd31d8bcce2d
Tianyi Wang <tianyi2358@gmail.com>
2023-11-07 00:54:17 +0100

    Bring back json transpose and move it to a safe location

--- eval/eval_dd_package.cpp
+++ eval/eval_dd_package.cpp
@@ -39,6 +39,90 @@ static const std::string FILENAME_REDUCED = "results_reduced.json";
 
 static constexpr std::size_t SEED = 42U;
 
+void transposeAndReduceJson(const std::string& fileName, const std::string& outFilename){
+  std::ifstream ifs(fileName);
+  nlohmann::json j;
+  ifs >> j;
+  ifs.close();
+
+  nlohmann::json k;
+
+  for (const auto& [algorithm, resultsA] : j.items()) {
+    for (const auto& [type, resultsT] : resultsA.items()) {
+      for (const auto& [nqubits, resultsN] : resultsT.items()) {
+        for (const auto& [branch, resultsB] : resultsN.items()) {
+          const auto& runtime = resultsB["runtime"];
+          k[algorithm][type][nqubits]["runtime"][branch] = runtime;
+
+          const auto& gateCount = resultsB["gate_count"];
+          k[algorithm][type][nqubits]["gate_count"][branch] = gateCount;
+
+          const auto& dd = resultsB["dd"];
+          const auto& activeMemoryMiB = dd["active_memory_mib"];
+          k[algorithm][type][nqubits]["dd"]["active_memory_mib"][branch] =
+              activeMemoryMiB;
+          const auto& peakMemoryMiB = dd["peak_memory_mib"];
+          k[algorithm][type][nqubits]["dd"]["peak_memory_mib"][branch] =
+              peakMemoryMiB;
+          for (const auto& stat : {"matrix", "vector", "density_matrix",
+                                   "real_numbers", "compute_tables"}) {
+            for (const auto& [key, value] : dd[stat].items()) {
+              if (value == "unused") {
+                k[algorithm][type][nqubits]["dd"][stat][key][branch] = value;
+                continue;
+              }
+
+              for (const auto& [key2, value2] : value.items()) {
+                if ((std::strcmp(stat, "matrix") != 0 ||
+                     std::strcmp(stat, "vector") != 0 ||
+                     std::strcmp(stat, "density_matrix") != 0) &&
+                    key == "unique_table") {
+                  for (const auto& [key3, value3] : value2.items()) {
+                    k[algorithm][type][nqubits]["dd"][stat][key][key2][key3]
+                      [branch] = value3;
+                  }
+                    continue;
+                }
+                k[algorithm][type][nqubits]["dd"][stat][key][key2][branch] =
+                    value2;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  for (const auto& [algorithm, resultsA] : k.items()) {
+    for (const auto& [type, resultsT] : resultsA.items()) {
+      for (const auto& [nqubits, resultsN] : resultsT.items()) {
+        auto& dd = resultsN["dd"];
+        dd.erase("density_matrix");
+
+        auto& computeTables = dd["compute_tables"];
+        computeTables.erase("density_matrix_add");
+        computeTables.erase("density_density_mult");
+        computeTables.erase("density_noise_operations");
+        computeTables.erase("stochastic_noise_operations");
+        computeTables.erase("matrix_kronecker");
+        computeTables.erase("vector_kronecker");
+        computeTables.erase("vector_inner_product");
+        computeTables.erase("matrix_conjugate_transpose");
+
+        if (type == "Functionality") {
+          dd.erase("vector");
+          computeTables.erase("vector_add");
+          computeTables.erase("matrix_vector_mult");
+        }
+      }
+    }
+  }
+
+  std::ofstream ofs(outFilename);
+  ofs << k.dump(2U);
+  ofs.close();
+}
+
 // a function that parses a nlohmann::json from a file "results.json", populates
 // it with the results of the current run and writes it back to the file
 
@@ -72,6 +156,8 @@ void verifyAndSave(const std::string& name, const std::string& type,
   std::ofstream ofs(FILENAME);
   ofs << j.dump(2U);
   ofs.close();
+
+  transposeAndReduceJson(FILENAME, FILENAME_REDUCED);
 }
 
 class GHZEval : public testing::TestWithParam<std::size_t> {
