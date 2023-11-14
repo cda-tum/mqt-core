04553f9335cc6a7183e309d68ec3d3476a5c1a47
Tianyi Wang <tianyi2358@gmail.com>
2023-11-14 13:07:06 +0100

    Fix path of example results

--- test/python/test_eval_json.py
+++ test/python/test_eval_json.py
@@ -2,29 +2,34 @@
 
 from __future__ import annotations
 
+from pathlib import Path
 from mqt.core.evaluation import compare
+
+path = Path(__file__).resolve().parent / "example_results.json"
+
+
 
 
 def test_zero_point_one() -> None:
     """Test factor 0.1."""
-    compare("example_results.json", factor=0.1, only_changed=False, sort="ratio", no_split=False)
+    compare(path, factor=0.1, only_changed=False, sort="ratio", no_split=False)
 
 
 def test_zero_point_three() -> None:
     """Test factor 0.3."""
-    compare("example_results.json", factor=0.3, only_changed=False, sort="ratio", no_split=False)
+    compare(path, factor=0.3, only_changed=False, sort="ratio", no_split=False)
 
 
 def test_only_changed() -> None:
     """Test only changed."""
-    compare("example_results.json", factor=0.2, only_changed=True, sort="ratio", no_split=False)
+    compare(path, factor=0.2, only_changed=True, sort="ratio", no_split=False)
 
 
 def test_only_changed_and_no_split() -> None:
     """Test only changed and no split."""
-    compare("example_results.json", factor=0.2, only_changed=True, sort="ratio", no_split=True)
+    compare(path, factor=0.2, only_changed=True, sort="ratio", no_split=True)
 
 
 def test_sort_by_experiment() -> None:
     """Test sort by experiment."""
-    compare("example_results.json", factor=0.2, only_changed=True, sort="experiment", no_split=True)
+    compare(path, factor=0.2, only_changed=True, sort="experiment", no_split=True)
