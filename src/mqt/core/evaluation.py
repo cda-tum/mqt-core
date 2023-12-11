1fc289dc5f00cd84f17c05d531dfd73759c33b22
Tianyi Wang <tianyi2358@gmail.com>
2023-12-12 00:42:31 +0100

    Change alignment

--- src/mqt/core/evaluation.py
+++ src/mqt/core/evaluation.py
@@ -192,24 +192,25 @@ def compare(
             print("\nAll benchmarks:\n")
 
         df_all = df_all.sort_values(by=sort) if sort == "ratio" else df_all.sort_values([sort, "ratio"])
-        print(df_all.to_markdown(index=False))
+        print(df_all.to_markdown(index=False, stralign="right"))
         return
 
     print(f"\n{Bcolors.OKGREEN}Benchmarks that have improved:{Bcolors.ENDC}\n")
     df_improved = df_all[(m1 & ~m2) | (m3 & m2)]
     df_improved = df_improved.sort_values(by=sort) if sort == "ratio" else df_improved.sort_values([sort, "ratio"])
-    print(df_improved.to_markdown(index=False))
+    print(df_improved.to_markdown(index=False, stralign="right"))
 
     print(f"\n{Bcolors.FAIL}Benchmarks that have worsened:{Bcolors.ENDC}\n")
     df_worsened = df_all[(m3 & ~m2) | (m1 & m2)]
     df_worsened = df_worsened.sort_values(by=sort) if sort == "ratio" else df_worsened.sort_values([sort, "ratio"])
     print(df_worsened.to_markdown(index=False))
+    print(df_worsened.to_markdown(index=False, stralign="right"))
 
     if not only_changed:
         print("\nBenchmarks that have stayed the same:\n")
         df_same = df_all[m4]
         df_same = df_same.sort_values(by=sort) if sort == "ratio" else df_same.sort_values([sort, "ratio"])
-        print(df_same.to_markdown(index=False))
+        print(df_same.to_markdown(index=False, stralign="right"))
 
 
 def main() -> None:
