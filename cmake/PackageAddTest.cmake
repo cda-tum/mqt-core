aa29989e841638e9fbd915f3768c792c9762e419
burgholzer <burgholzer@me.com>
2024-01-16 12:39:03 +0100

    ♻️💥 privately link the `MQT::ProjectOptions` and `MQT::ProjectWarnings` targets
    Note that this will require upstream projects to explicitly link against these targets. This makes sure that no options are forced on targets that incorporate mqt-core
    
    Signed-off-by: burgholzer <burgholzer@me.com>

--- cmake/PackageAddTest.cmake
+++ cmake/PackageAddTest.cmake
@@ -4,7 +4,8 @@ macro(PACKAGE_ADD_TEST testname linklibs)
     # create an executable in which the tests will be stored
     add_executable(${testname} ${ARGN})
     # link the Google test infrastructure and a default main function to the test executable.
-    target_link_libraries(${testname} PRIVATE ${linklibs} gmock gtest_main)
+    target_link_libraries(${testname} PRIVATE ${linklibs} gmock gtest_main MQT::ProjectOptions
+                                              MQT::ProjectWarnings)
     # discover tests
     gtest_discover_tests(
       ${testname}
