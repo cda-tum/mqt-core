56b08c2392594b4a600831206abe29768c5d87d7
Berti Florea <56759700+BertiFlorea@users.noreply.github.com>
2024-01-17 12:36:42 +0100

    Update include/dd/Package.hpp
    Co-authored-by: Lukas Burgholzer <burgholzer@me.com>

--- include/dd/Package.hpp
+++ include/dd/Package.hpp
@@ -661,7 +661,7 @@ public:
       for (auto& row : em) {
         for (auto i1 = 0U; i1 < RADIX; ++i1) {
           for (auto i2 = 0U; i2 < RADIX; ++i2) {
-            auto i = i1 * RADIX + i2;
+            const auto i = i1 * RADIX + i2;
             if (it != controls.end() && it->qubit == z) {
               auto edges = std::array{mEdge::zero(), mEdge::zero(),
                                       mEdge::zero(), mEdge::zero()};
