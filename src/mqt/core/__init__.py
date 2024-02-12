73ca1ad37f4d0a6689dbb0fb1970df9f93b9daae
Lukas Burgholzer <burgholzer@me.com>
2024-02-12 12:07:10 +0100

    🔧 properly set Windows DLL path
    Signed-off-by: Lukas Burgholzer <burgholzer@me.com>

--- src/mqt/core/__init__.py
+++ src/mqt/core/__init__.py
@@ -2,6 +2,17 @@
 
 from __future__ import annotations
 
+import sys
+
+# under Windows, make sure to add the appropriate DLL directory to the PATH
+if sys.platform == "win32":
+    import os
+    import sysconfig
+    from pathlib import Path
+
+    bin_dir = Path(sysconfig.get_paths()["purelib"]) / "mqt" / "core" / "bin"
+    os.add_dll_directory(str(bin_dir))
+
 from ._core import Permutation, QuantumComputation
 from ._version import version as __version__
 from ._version import version_tuple as version_info
