7315bd1cc6b7dd05ce68e8f7f3f65ebcf95cc925
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-04-18 13:47:44 +0200

    ♻️ Incorporate NA operations into MQT-core

---
+++ include/mqt-core/na/NAComputation.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include "na/operations/NAOperation.hpp"
+#include "na/NADefinitions.hpp"
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace na {
+class NAComputation {
+protected:
+  std::vector<std::shared_ptr<Point>>       initialPositions;
+  std::vector<std::unique_ptr<NAOperation>> operations;
+
+public:
+  NAComputation()                                              = default;
+  NAComputation(NAComputation&& qc) noexcept            = default;
+  NAComputation& operator=(NAComputation&& qc) noexcept = default;
+  NAComputation(const NAComputation& qc)
+      : initialPositions(qc.initialPositions) {
+    operations.reserve(qc.operations.size());
+    std::transform(qc.operations.cbegin(), qc.operations.cend(),
+                   std::back_inserter(operations),
+                   [](const auto& op) { return op->clone(); });
+  }
+  NAComputation& operator=(const NAComputation& qc) {
+    if (this != &qc) {
+      initialPositions = qc.initialPositions;
+      operations.clear();
+      operations.reserve(qc.operations.size());
+      std::transform(qc.operations.cbegin(), qc.operations.cend(),
+                     std::back_inserter(operations),
+                     [](const auto& op) { return op->clone(); });
+    }
+    return *this;
+  }
+  virtual ~NAComputation() = default;
+  template <class T> auto emplaceBack(std::unique_ptr<T>&& op) -> void {
+    static_assert(std::is_base_of<NAOperation, T>::value,
+                  "T must be a subclass of NAOperation.");
+    operations.emplace_back(std::move(op));
+  }
+  template <class T> auto emplaceBack(const std::unique_ptr<T>& op) -> void {
+    static_assert(std::is_base_of<NAOperation, T>::value,
+                  "T must be a subclass of NAOperation.");
+    operations.emplace_back(std::move(op));
+  }
+  template <class T, class... Args> auto emplaceBack(Args&&... args) -> void {
+    static_assert(std::is_base_of<NAOperation, T>::value,
+                  "T must be a subclass of NAOperation.");
+    operations.emplace_back(std::make_unique<T>(args...));
+  }
+  auto clear() -> void { operations.clear(); }
+  auto clearInitialPositions() -> void { initialPositions.clear(); }
+  [[nodiscard]] auto size() const -> std::size_t { return operations.size(); }
+  [[nodiscard]] auto getInitialPositions() const
+      -> const std::vector<std::shared_ptr<Point>>& {
+    return initialPositions;
+  }
+  auto emplaceInitialPosition(std::shared_ptr<Point> p) -> void {
+    initialPositions.emplace_back(std::move(p));
+  }
+  [[nodiscard]] auto toString() const -> std::string {
+    std::stringstream ss;
+    ss << "init at ";
+    for (const auto& p : initialPositions) {
+      ss << "(" << p->x << ", " << p->y << ")"
+         << ", ";
+    }
+    ss.seekp(-2, std::ios_base::end);
+    ss << ";\n";
+    for (const auto& op : operations) {
+      ss << op->toString();
+    }
+    return ss.str();
+  }
+  friend auto operator<<(std::ostream& os, const NAComputation& qc)
+      -> std::ostream& {
+    os << qc.toString();
+    return os;
+  }
+  // Iterators (pass-through)
+  auto               begin() noexcept { return operations.begin(); }
+  [[nodiscard]] auto begin() const noexcept { return operations.begin(); }
+  [[nodiscard]] auto cbegin() const noexcept { return operations.cbegin(); }
+  auto               end() noexcept { return operations.end(); }
+  [[nodiscard]] auto end() const noexcept { return operations.end(); }
+  [[nodiscard]] auto cend() const noexcept { return operations.cend(); }
+  auto               rbegin() noexcept { return operations.rbegin(); }
+  [[nodiscard]] auto rbegin() const noexcept { return operations.rbegin(); }
+  [[nodiscard]] auto crbegin() const noexcept { return operations.crbegin(); }
+  auto               rend() noexcept { return operations.rend(); }
+  [[nodiscard]] auto rend() const noexcept { return operations.rend(); }
+  [[nodiscard]] auto crend() const noexcept { return operations.crend(); }
+};
+} // namespace na
---
+++ test/na/test_nacomputation.cpp
@@ -0,0 +1,53 @@
+//
+// This file is part of the MQT QMAP library released under the MIT license.
+// See README.md or go to https://github.com/cda-tum/qmap for more information.
+//
+
+#include "na/NAComputation.hpp"
+#include "na/NADefinitions.hpp"
+#include "na/operations/NAGlobalOperation.hpp"
+#include "na/operations/NALocalOperation.hpp"
+#include "na/operations/NAShuttlingOperation.hpp"
+#include "operations/OpType.hpp"
+
+#include "gtest/gtest.h"
+#include <sstream>
+#include <string>
+
+TEST(NAComputation, General) {
+  auto qc = na::NAComputation();
+  qc.emplaceInitialPosition(std::make_shared<na::Point>(0, 0));
+  qc.emplaceInitialPosition(std::make_shared<na::Point>(1, 0));
+  qc.emplaceInitialPosition(std::make_shared<na::Point>(2, 0));
+  qc.emplaceBack(std::make_unique<na::NALocalOperation>(
+      na::OpType{qc::RZ, 0}, std::vector<qc::fp>{qc::PI_2},
+      std::make_shared<na::Point>(0, 0)));
+  qc.emplaceBack(std::make_unique<na::NAGlobalOperation>(
+      na::OpType{qc::RY, 0}, std::vector<qc::fp>{qc::PI_2}));
+  qc.emplaceBack(std::make_unique<na::NAShuttlingOperation>(
+      na::ShuttleType::LOAD,
+      std::vector{std::make_shared<na::Point>(0, 0),
+                  std::make_shared<na::Point>(1, 0)},
+      std::vector{std::make_shared<na::Point>(0, 1),
+                  std::make_shared<na::Point>(1, 1)}));
+  qc.emplaceBack(std::make_unique<na::NAShuttlingOperation>(
+      na::ShuttleType::MOVE,
+      std::vector{std::make_shared<na::Point>(0, 1),
+                  std::make_shared<na::Point>(1, 1)},
+      std::vector{std::make_shared<na::Point>(4, 1),
+                  std::make_shared<na::Point>(5, 1)}));
+  qc.emplaceBack(std::make_unique<na::NAShuttlingOperation>(
+      na::ShuttleType::STORE,
+      std::vector{std::make_shared<na::Point>(4, 1),
+                  std::make_shared<na::Point>(5, 1)},
+      std::vector{std::make_shared<na::Point>(4, 0),
+                  std::make_shared<na::Point>(5, 0)}));
+  std::stringstream ss;
+  ss << qc;
+  EXPECT_EQ(ss.str(), "init at (0, 0), (1, 0), (2, 0);\n"
+                      "rz(1.5708) at (0, 0);\n"
+                      "ry(1.5708);\n"
+                      "load (0, 0), (1, 0) to (0, 1), (1, 1);\n"
+                      "move (0, 1), (1, 1) to (4, 1), (5, 1);\n"
+                      "store (4, 1), (5, 1) to (4, 0), (5, 0);\n");
+}
