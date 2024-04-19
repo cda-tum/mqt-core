92f548cd79a901ddee28c2b005ac30159903346d
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-04-19 14:28:51 +0200

    ♻️ Add DisjointSet, Fix CMakeLists

---
+++ include/mqt-core/DisjointSet.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <algorithm>
+#include <unordered_map>
+
+template <class T> struct DisjointSet {
+  std::unordered_map<T, T>           parent;
+  std::unordered_map<T, std::size_t> rank;
+
+  template <class Iterator>
+  explicit DisjointSet(const Iterator& begin, const Iterator& end) {
+    std::for_each(begin, end, [&](const auto& element) {
+      parent[element] = element;
+      rank[element]   = 0;
+    });
+  }
+
+  T findSet(T v) {
+    if (parent[v] != v) {
+      parent[v] = findSet(parent[v]);
+    }
+    return parent[v];
+  }
+
+  void unionSet(T x, T y) {
+    x = findSet(x);
+    y = findSet(y);
+    if (rank[x] > rank[y]) {
+      parent[y] = x;
+    } else {
+      parent[x] = y;
+      if (rank[x] == rank[y]) {
+        ++rank[y];
+      }
+    }
+  }
+};
