ee9c10557c74417bac949524004011eeda1a003b
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-04-22 14:03:25 +0200

    ♻️ Add disjoint set to MQT core

---
+++ test/test_disjoint_set.cpp
@@ -0,0 +1,29 @@
+//
+// This file is part of the MQT QMAP library released under the MIT license.
+// See README.md or go to https://github.com/cda-tum/mqt-core for more
+// information.
+//
+
+#include "DisjointSet.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(DisjointSet, FindSet) {
+  std::vector elements = {1, 2, 3};
+  DisjointSet<int> ds(elements.begin(), elements.end());
+  EXPECT_EQ(ds.findSet(1), ds.findSet(1));
+  EXPECT_NE(ds.findSet(1), ds.findSet(2));
+}
+
+TEST(DisjointSet, UnionSet) {
+  std::vector elements = {1, 2, 3};
+  DisjointSet<int> ds(elements.begin(), elements.end());
+  ds.unionSet(1, 2);
+  EXPECT_EQ(ds.findSet(1), ds.findSet(2));
+  EXPECT_NE(ds.findSet(1), ds.findSet(3));
+  EXPECT_NE(ds.findSet(2), ds.findSet(3));
+  ds.unionSet(1, 3);
+  EXPECT_EQ(ds.findSet(1), ds.findSet(2));
+  EXPECT_EQ(ds.findSet(1), ds.findSet(3));
+  EXPECT_EQ(ds.findSet(2), ds.findSet(3));
+}
