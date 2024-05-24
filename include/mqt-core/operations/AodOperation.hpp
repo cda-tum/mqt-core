0d3e162182fb10c5e6858e05e45279f1469a762d
Ludwig Schmid <ludwig.s.schmid@tum.de>
2024-05-24 16:07:13 +0200

    🐛 Added missing header files

--- include/mqt-core/operations/AodOperation.hpp
+++ include/mqt-core/operations/AodOperation.hpp
@@ -1,14 +1,10 @@
-//
-// Created by Ludwig Schmid on 30.10.23.
-//
-
 #pragma once
 
 #include "operations/Operation.hpp"
 
 namespace qc {
 
-enum class Dimension { X = 0, Y = 1 };
+enum class Dimension : std::uint8_t { X = 0, Y = 1 };
 struct SingleOperation {
   Dimension dir;
   fp start;
@@ -17,7 +13,7 @@ struct SingleOperation {
   SingleOperation(Dimension dir, fp start, fp end)
       : dir(dir), start(start), end(end) {}
 
-  std::string toQASMString() const {
+  [[nodiscard]] std::string toQASMString() const {
     std::stringstream ss;
     ss << static_cast<char32_t>(dir) << ", " << start << ", " << end << "; ";
     return ss.str();
@@ -26,16 +22,18 @@ struct SingleOperation {
 class AodOperation : public Operation {
   std::vector<SingleOperation> operations;
 
+  static std::vector<Dimension> convertToDimension(const std::vector<uint32_t>& dirs) ;
+
 public:
   AodOperation() = default;
   AodOperation(OpType type, std::vector<Qubit> targets,
                std::vector<Dimension> dirs, std::vector<fp> starts,
                std::vector<fp> ends);
   AodOperation(OpType type, std::vector<Qubit> targets,
-               std::vector<uint32_t> dirs, std::vector<fp> starts,
+               const std::vector<uint32_t>& dirs, std::vector<fp> starts,
                std::vector<fp> ends);
-  AodOperation(std::string type, std::vector<Qubit> targets,
-               std::vector<uint32_t> dirs, std::vector<fp> starts,
+  AodOperation(const std::string& type, std::vector<Qubit> targets,
+               const std::vector<uint32_t>& dirs, std::vector<fp> starts,
                std::vector<fp> ends);
   AodOperation(OpType type, std::vector<Qubit> targets,
                std::vector<std::tuple<Dimension, fp, fp>>& operations);
@@ -53,7 +51,7 @@ public:
     return it;
   }
 
-  [[nodiscard]] std::vector<fp> inline getEnds(Dimension dir) const {
+  [[nodiscard]] std::vector<fp> getEnds(Dimension dir) const {
     std::vector<fp> ends;
     for (const auto& op : operations) {
       if (op.dir == dir) {
@@ -63,7 +61,7 @@ public:
     return ends;
   }
 
-  [[nodiscard]] std::vector<fp> inline getStarts(Dimension dir) const {
+  [[nodiscard]] std::vector<fp> getStarts(Dimension dir) const {
     std::vector<fp> starts;
     for (const auto& op : operations) {
       if (op.dir == dir) {
@@ -73,7 +71,7 @@ public:
     return starts;
   }
 
-  [[nodiscard]] fp inline getMaxDistance(Dimension dir) const {
+  [[nodiscard]] fp getMaxDistance(Dimension dir) const {
     const auto distances = getDistances(dir);
     if (distances.empty()) {
       return 0;
@@ -81,7 +79,7 @@ public:
     return *std::max_element(distances.begin(), distances.end());
   }
 
-  [[nodiscard]] std::vector<fp> inline getDistances(Dimension dir) const {
+  [[nodiscard]] std::vector<fp> getDistances(Dimension dir) const {
     std::vector<fp> params;
     for (const auto& op : operations) {
       if (op.dir == dir) {
--- src/operations/AodOperation.cpp
+++ src/operations/AodOperation.cpp
@@ -1,24 +1,26 @@
-//
-// Created by Ludwig Schmid on 30.10.23.
-//
-
 #include "operations/AodOperation.hpp"
+#include "Definitions.hpp"
 
 #include "cassert"
+#include "iomanip"
+#include "limits"
 
 #include <utility>
 
 namespace qc {
 AodOperation::AodOperation(OpType s, std::vector<Qubit> qubits,
-                           std::vector<uint32_t> dirs, std::vector<fp> start,
-                           std::vector<fp> end) {
-  auto dirsEnum = std::vector<Dimension>(dirs.size());
+                           const std::vector<uint32_t>& dirs, std::vector<fp> start,
+                           std::vector<fp> end)
+    : AodOperation(s, std::move(qubits), convertToDimension(dirs), std::move(start), std::move(end)) {}
+
+std::vector<Dimension> AodOperation::convertToDimension(const std::vector<uint32_t>& dirs) {
+  std::vector<Dimension> dirsEnum(dirs.size());
   for (size_t i = 0; i < dirs.size(); ++i) {
     dirsEnum[i] = static_cast<Dimension>(dirs[i]);
   }
-  AodOperation(s, std::move(qubits), std::move(dirsEnum), std::move(start),
-               std::move(end));
+  return dirsEnum;
 }
+
 
 AodOperation::AodOperation(OpType s, std::vector<Qubit> qubits,
                            std::vector<Dimension> dirs, std::vector<fp> start,
@@ -33,11 +35,11 @@ AodOperation::AodOperation(OpType s, std::vector<Qubit> qubits,
   }
 }
 
-AodOperation::AodOperation(std::string type, std::vector<Qubit> targets,
-                           std::vector<uint32_t> dirs, std::vector<fp> start,
+AodOperation::AodOperation(const std::string& type, std::vector<Qubit> targets,
+                           const std::vector<uint32_t>& dirs, std::vector<fp> start,
                            std::vector<fp> end)
     : AodOperation(OP_NAME_TO_TYPE.at(type), std::move(targets),
-                   std::move(dirs), std::move(start), std::move(end)) {}
+                   dirs, std::move(start), std::move(end)) {}
 
 AodOperation::AodOperation(
     OpType s, std::vector<Qubit> targets,
@@ -55,7 +57,7 @@ AodOperation::AodOperation(OpType s, std::vector<Qubit> t,
                            std::vector<SingleOperation>& ops)
     : operations(std::move(ops)) {
   type = s;
-  this->targets = t;
+  this->targets = std::move(t);
   name = toString(type);
 }
 
