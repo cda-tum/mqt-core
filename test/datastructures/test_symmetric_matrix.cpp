2a43c6966b7308ff7065a36e981d2ae4a0cd73be
lsschmid <117631861+lsschmid@users.noreply.github.com>
2024-06-03 09:37:39 +0200

    Moved symmetric matrix to datastructures (#621)
    ## Description
    
    Moved the datastructure "Symmtric Matrix" from qmap to mqt-core as it
    might be useful also in other situations.
    
    ## Checklist:
    
    - [x] The pull request only contains commits that are related to it.
    - [x] I have added appropriate tests and documentation.
    - [x] I have made sure that all CI jobs on GitHub pass.
    - [x] The pull request introduces no new warnings and follows the
    project's style guidelines.

---
+++ test/datastructures/test_symmetric_matrix.cpp
@@ -0,0 +1,48 @@
+#include "datastructures/SymmetricMatrix.hpp"
+
+#include <cstddef>
+#include <gtest/gtest.h>
+#include <string>
+
+namespace qc {
+
+TEST(SymmetricMatrix, Constructors) {
+  SymmetricMatrix<int> const m(3);
+  EXPECT_EQ(m.size(), 3);
+  SymmetricMatrix<int> m2(3, 1);
+  EXPECT_EQ(m2.size(), 3);
+  for (size_t i = 0; i < m2.size(); ++i) {
+    for (size_t j = 0; j <= i; ++j) {
+      EXPECT_EQ(m2(i, j), 1);
+      EXPECT_EQ(m2(j, i), 1);
+    }
+  }
+}
+
+TEST(SymmetricMatrix, DifferentDataTypes) {
+  SymmetricMatrix<double> const m(3);
+  EXPECT_EQ(m.size(), 3);
+  SymmetricMatrix<std::string> const m2(3, "1");
+  EXPECT_EQ(m2.size(), 3);
+  EXPECT_EQ(m2(0, 2), m2(2, 0));
+  EXPECT_EQ(m2(0, 2), "1");
+  SymmetricMatrix<char> const m3(3, '1');
+  EXPECT_EQ(m3.size(), 3);
+  EXPECT_EQ(m3(0, 1), m3(1, 0));
+  EXPECT_EQ(m3(0, 1), '1');
+}
+
+TEST(SymmetricMatrix, Assignment) {
+  SymmetricMatrix<int> m(3);
+  m(0, 1) = 1;
+  m(1, 2) = 2;
+  m(0, 2) = 3;
+  EXPECT_EQ(m(0, 1), 1);
+  EXPECT_EQ(m(1, 0), 1);
+  EXPECT_EQ(m(1, 2), 2);
+  EXPECT_EQ(m(2, 1), 2);
+  EXPECT_EQ(m(0, 2), 3);
+  EXPECT_EQ(m(2, 0), 3);
+}
+
+} // namespace qc
