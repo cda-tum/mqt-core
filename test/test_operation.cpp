fe0ebffbf617e47a4a850e02d5c93bced3eee0f3
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-07-01 10:43:28 +0200

    🧪 Add failing test for fromQASM with Bench Circuit

--- test/test_operation.cpp
+++ test/test_operation.cpp
@@ -8,6 +8,7 @@
 #include "operations/StandardOperation.hpp"
 #include "operations/SymbolicOperation.hpp"
 
+#include <QuantumComputation.hpp>
 #include <cstdint>
 #include <gtest/gtest.h>
 #include <sstream>
@@ -257,4 +258,30 @@ TEST(AodOperation, Invert) {
                               {na::Dimension::X}, {0.0}, {1.0});
   deactivate.invert();
   EXPECT_EQ(deactivate.getType(), qc::OpType::AodActivate);
+}
+
+TEST(QuantumComputation, fromQASM) {
+  const std::string qasm = R"(
+    // Benchmark was created by MQT Bench on 2024-03-17
+    // For more information about MQT Bench, please visit https://www.cda.cit.tum.de/mqtbench/
+    // MQT Bench version: 1.1.0
+    // Qiskit version: 1.0.2
+
+    OPENQASM 2.0;
+    include "qelib1.inc";
+    qreg eval[1];
+    qreg q[1];
+    creg meas[2];
+    u2(0,-pi) eval[0];
+    u3(0.9272952180016122,0,0) q[0];
+    cx eval[0],q[0];
+    u(-0.9272952180016122,0,0) q[0];
+    cx eval[0],q[0];
+    h eval[0];
+    u(0.9272952180016122,0,0) q[0];
+    barrier eval[0],q[0];
+    measure eval[0] -> meas[0];
+    measure q[0] -> meas[1];
+  )";
+  EXPECT_NO_THROW(std::ignore = qc::QuantumComputation::fromQASM(qasm));
 }
