cc0ccf891167562cb2ca025ec7ab138e0fcdbb9d
joshy <janez.rotman@tum.de>
2024-12-15 21:15:55 +0100

    :recycle: move helpers in GateMatrixDefinition from .hpp to .cpp

--- include/mqt-core/dd/GateMatrixDefinitions.hpp
+++ include/mqt-core/dd/GateMatrixDefinitions.hpp
@@ -12,9 +12,6 @@
 #include "dd/DDDefinitions.hpp"
 #include "ir/operations/OpType.hpp"
 
-#include <cmath>
-#include <complex>
-#include <unordered_map>
 #include <vector>
 
 namespace dd {
@@ -23,132 +20,10 @@ namespace dd {
 constexpr GateMatrix MEAS_ZERO_MAT{1, 0, 0, 0};
 constexpr GateMatrix MEAS_ONE_MAT{0, 0, 0, 1};
 
-extern const std::unordered_map<qc::OpType,
-                                GateMatrix (*const)(const std::vector<fp>&)>
-    MATS_GENERATORS;
+GateMatrix opToSingleQubitGateMatrix(qc::OpType t,
+                                     const std::vector<fp>& params = {});
 
-extern const std::unordered_map<qc::OpType, TwoQubitGateMatrix (*const)(
-                                                const std::vector<fp>&)>
-    TWO_GATE_MATS_GENERATORS;
-
-inline GateMatrix uMat(const fp lambda, const fp phi, const fp theta) {
-  return GateMatrix{{{std::cos(theta / 2.), 0.},
-                     {-std::cos(lambda) * std::sin(theta / 2.),
-                      -std::sin(lambda) * std::sin(theta / 2.)},
-                     {std::cos(phi) * std::sin(theta / 2.),
-                      std::sin(phi) * std::sin(theta / 2.)},
-                     {std::cos(lambda + phi) * std::cos(theta / 2.),
-                      std::sin(lambda + phi) * std::cos(theta / 2.)}}};
-}
-
-inline GateMatrix u2Mat(const fp lambda, const fp phi) {
-  return GateMatrix{
-      SQRT2_2,
-      {-std::cos(lambda) * SQRT2_2, -std::sin(lambda) * SQRT2_2},
-      {std::cos(phi) * SQRT2_2, std::sin(phi) * SQRT2_2},
-      {std::cos(lambda + phi) * SQRT2_2, std::sin(lambda + phi) * SQRT2_2}};
-}
-
-inline GateMatrix pMat(const fp lambda) {
-  return GateMatrix{1, 0, 0, {std::cos(lambda), std::sin(lambda)}};
-}
-
-inline GateMatrix rxMat(const fp lambda) {
-  return GateMatrix{{{std::cos(lambda / 2.), 0.},
-                     {0., -std::sin(lambda / 2.)},
-                     {0., -std::sin(lambda / 2.)},
-                     {std::cos(lambda / 2.), 0.}}};
-}
-
-inline GateMatrix ryMat(const fp lambda) {
-  return GateMatrix{{{std::cos(lambda / 2.), 0.},
-                     {-std::sin(lambda / 2.), 0.},
-                     {std::sin(lambda / 2.), 0.},
-                     {std::cos(lambda / 2.), 0.}}};
-}
-
-inline GateMatrix rzMat(const fp lambda) {
-  return GateMatrix{{{std::cos(lambda / 2.), -std::sin(lambda / 2.)},
-                     0,
-                     0,
-                     {std::cos(lambda / 2.), std::sin(lambda / 2.)}}};
-}
-
-inline TwoQubitGateMatrix rxxMat(const fp theta) {
-  const auto cosTheta = std::cos(theta / 2.);
-  const auto sinTheta = std::sin(theta / 2.);
-
-  return TwoQubitGateMatrix{{{cosTheta, 0, 0, {0., -sinTheta}},
-                             {0, cosTheta, {0., -sinTheta}, 0},
-                             {0, {0., -sinTheta}, cosTheta, 0},
-                             {std::complex{0., -sinTheta}, 0, 0, cosTheta}}};
-}
-
-inline TwoQubitGateMatrix ryyMat(const fp theta) {
-  const auto cosTheta = std::cos(theta / 2.);
-  const auto sinTheta = std::sin(theta / 2.);
-
-  return TwoQubitGateMatrix{{{cosTheta, 0, 0, {0., sinTheta}},
-                             {0, cosTheta, {0., -sinTheta}, 0},
-                             {0, {0., -sinTheta}, cosTheta, 0},
-                             {std::complex{0., sinTheta}, 0, 0, cosTheta}}};
-}
-
-inline TwoQubitGateMatrix rzzMat(const fp theta) {
-  const auto cosTheta = std::cos(theta / 2.);
-  const auto sinTheta = std::sin(theta / 2.);
-
-  return TwoQubitGateMatrix{{{std::complex{cosTheta, -sinTheta}, 0, 0, 0},
-                             {0, {cosTheta, sinTheta}, 0, 0},
-                             {0, 0, {cosTheta, sinTheta}, 0},
-                             {0, 0, 0, {cosTheta, -sinTheta}}}};
-}
-
-inline TwoQubitGateMatrix rzxMat(const fp theta) {
-  const auto cosTheta = std::cos(theta / 2.);
-  const auto sinTheta = std::sin(theta / 2.);
-
-  return TwoQubitGateMatrix{{{cosTheta, {0., -sinTheta}, 0, 0},
-                             {std::complex{0., -sinTheta}, cosTheta, 0, 0},
-                             {0, 0, cosTheta, {0., sinTheta}},
-                             {0, 0, {0., sinTheta}, cosTheta}}};
-}
-
-inline TwoQubitGateMatrix xxMinusYYMat(const fp theta, const fp beta = 0.) {
-  const auto cosTheta = std::cos(theta / 2.);
-  const auto sinTheta = std::sin(theta / 2.);
-  const auto cosBeta = std::cos(beta);
-  const auto sinBeta = std::sin(beta);
-
-  return TwoQubitGateMatrix{
-      {{cosTheta, 0, 0, {-sinBeta * sinTheta, -cosBeta * sinTheta}},
-       {0, 1, 0, 0},
-       {0, 0, 1, 0},
-       {std::complex{sinBeta * sinTheta, -cosBeta * sinTheta}, 0, 0,
-        cosTheta}}};
-}
-
-inline TwoQubitGateMatrix xxPlusYYMat(const fp theta, const fp beta = 0.) {
-  const auto cosTheta = std::cos(theta / 2.);
-  const auto sinTheta = std::sin(theta / 2.);
-  const auto cosBeta = std::cos(beta);
-  const auto sinBeta = std::sin(beta);
-
-  return TwoQubitGateMatrix{
-      {{1, 0, 0, 0},
-       {0, cosTheta, {sinBeta * sinTheta, -cosBeta * sinTheta}, 0},
-       {0, {-sinBeta * sinTheta, -cosBeta * sinTheta}, cosTheta, 0},
-       {0, 0, 0, 1}}};
-}
-
-inline GateMatrix opToSingleQubitGateMatrix(qc::OpType t,
-                                       const std::vector<fp>& params = {}) {
-  return MATS_GENERATORS.at(t)(params);
-}
-
-inline TwoQubitGateMatrix
-opToTwoQubitGateMatrix(qc::OpType t, const std::vector<fp>& params = {}) {
-  return TWO_GATE_MATS_GENERATORS.at(t)(params);
-}
+TwoQubitGateMatrix opToTwoQubitGateMatrix(qc::OpType t,
+                                          const std::vector<fp>& params = {});
 
 } // namespace dd
--- src/dd/GateMatrixDefinitions.cpp
+++ src/dd/GateMatrixDefinitions.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Chair for Design Automation, TUM
+ * Copyright (c) 2025 Chair for Design Automation, TUM
  * All rights reserved.
  *
  * SPDX-License-Identifier: MIT
@@ -9,59 +9,218 @@
 
 #include "dd/GateMatrixDefinitions.hpp"
 
+#include "Definitions.hpp"
 #include "dd/DDDefinitions.hpp"
 #include "ir/operations/OpType.hpp"
 
+#include <cassert>
+#include <cmath>
 #include <complex>
-#include <unordered_map>
 #include <vector>
+
+namespace {
+using namespace dd;
+
+inline GateMatrix uMat(const fp lambda, const fp phi, const fp theta) {
+  return GateMatrix{{{std::cos(theta / 2.), 0.},
+                     {-std::cos(lambda) * std::sin(theta / 2.),
+                      -std::sin(lambda) * std::sin(theta / 2.)},
+                     {std::cos(phi) * std::sin(theta / 2.),
+                      std::sin(phi) * std::sin(theta / 2.)},
+                     {std::cos(lambda + phi) * std::cos(theta / 2.),
+                      std::sin(lambda + phi) * std::cos(theta / 2.)}}};
+}
+
+inline GateMatrix u2Mat(const fp lambda, const fp phi) {
+  return GateMatrix{
+      SQRT2_2,
+      {-std::cos(lambda) * SQRT2_2, -std::sin(lambda) * SQRT2_2},
+      {std::cos(phi) * SQRT2_2, std::sin(phi) * SQRT2_2},
+      {std::cos(lambda + phi) * SQRT2_2, std::sin(lambda + phi) * SQRT2_2}};
+}
+
+inline GateMatrix pMat(const fp lambda) {
+  return GateMatrix{1, 0, 0, {std::cos(lambda), std::sin(lambda)}};
+}
+
+inline GateMatrix rxMat(const fp lambda) {
+  return GateMatrix{{{std::cos(lambda / 2.), 0.},
+                     {0., -std::sin(lambda / 2.)},
+                     {0., -std::sin(lambda / 2.)},
+                     {std::cos(lambda / 2.), 0.}}};
+}
+
+inline GateMatrix ryMat(const fp lambda) {
+  return GateMatrix{{{std::cos(lambda / 2.), 0.},
+                     {-std::sin(lambda / 2.), 0.},
+                     {std::sin(lambda / 2.), 0.},
+                     {std::cos(lambda / 2.), 0.}}};
+}
+
+inline GateMatrix rzMat(const fp lambda) {
+  return GateMatrix{{{std::cos(lambda / 2.), -std::sin(lambda / 2.)},
+                     0,
+                     0,
+                     {std::cos(lambda / 2.), std::sin(lambda / 2.)}}};
+}
+
+inline TwoQubitGateMatrix rxxMat(const fp theta) {
+  const auto cosTheta = std::cos(theta / 2.);
+  const auto sinTheta = std::sin(theta / 2.);
+
+  return TwoQubitGateMatrix{{{cosTheta, 0, 0, {0., -sinTheta}},
+                             {0, cosTheta, {0., -sinTheta}, 0},
+                             {0, {0., -sinTheta}, cosTheta, 0},
+                             {std::complex{0., -sinTheta}, 0, 0, cosTheta}}};
+}
+
+inline TwoQubitGateMatrix ryyMat(const fp theta) {
+  const auto cosTheta = std::cos(theta / 2.);
+  const auto sinTheta = std::sin(theta / 2.);
+
+  return TwoQubitGateMatrix{{{cosTheta, 0, 0, {0., sinTheta}},
+                             {0, cosTheta, {0., -sinTheta}, 0},
+                             {0, {0., -sinTheta}, cosTheta, 0},
+                             {std::complex{0., sinTheta}, 0, 0, cosTheta}}};
+}
+
+inline TwoQubitGateMatrix rzzMat(const fp theta) {
+  const auto cosTheta = std::cos(theta / 2.);
+  const auto sinTheta = std::sin(theta / 2.);
+
+  return TwoQubitGateMatrix{{{std::complex{cosTheta, -sinTheta}, 0, 0, 0},
+                             {0, {cosTheta, sinTheta}, 0, 0},
+                             {0, 0, {cosTheta, sinTheta}, 0},
+                             {0, 0, 0, {cosTheta, -sinTheta}}}};
+}
+
+inline TwoQubitGateMatrix rzxMat(const fp theta) {
+  const auto cosTheta = std::cos(theta / 2.);
+  const auto sinTheta = std::sin(theta / 2.);
+
+  return TwoQubitGateMatrix{{{cosTheta, {0., -sinTheta}, 0, 0},
+                             {std::complex{0., -sinTheta}, cosTheta, 0, 0},
+                             {0, 0, cosTheta, {0., sinTheta}},
+                             {0, 0, {0., sinTheta}, cosTheta}}};
+}
+
+inline TwoQubitGateMatrix xxMinusYYMat(const fp theta, const fp beta = 0.) {
+  const auto cosTheta = std::cos(theta / 2.);
+  const auto sinTheta = std::sin(theta / 2.);
+  const auto cosBeta = std::cos(beta);
+  const auto sinBeta = std::sin(beta);
+
+  return TwoQubitGateMatrix{
+      {{cosTheta, 0, 0, {-sinBeta * sinTheta, -cosBeta * sinTheta}},
+       {0, 1, 0, 0},
+       {0, 0, 1, 0},
+       {std::complex{sinBeta * sinTheta, -cosBeta * sinTheta}, 0, 0,
+        cosTheta}}};
+}
+
+inline TwoQubitGateMatrix xxPlusYYMat(const fp theta, const fp beta = 0.) {
+  const auto cosTheta = std::cos(theta / 2.);
+  const auto sinTheta = std::sin(theta / 2.);
+  const auto cosBeta = std::cos(beta);
+  const auto sinBeta = std::sin(beta);
+
+  return TwoQubitGateMatrix{
+      {{1, 0, 0, 0},
+       {0, cosTheta, {sinBeta * sinTheta, -cosBeta * sinTheta}, 0},
+       {0, {-sinBeta * sinTheta, -cosBeta * sinTheta}, cosTheta, 0},
+       {0, 0, 0, 1}}};
+}
+} // namespace
 
 namespace dd {
 
-const std::unordered_map<qc::OpType,
-                         GateMatrix (*const)(const std::vector<fp>&)>
-    MATS_GENERATORS{{
-        // clang-format off
-        {qc::I,   [](const std::vector<fp>&) -> GateMatrix { return {1, 0, 0, 1}; }},
-        {qc::H,   [](const std::vector<fp>&) -> GateMatrix { return {SQRT2_2, SQRT2_2, SQRT2_2, -SQRT2_2}; }},
-        {qc::X,   [](const std::vector<fp>&) -> GateMatrix { return {0, 1, 1, 0}; }},
-        {qc::Y,   [](const std::vector<fp>&) -> GateMatrix { return {0, {0, -1}, {0, 1}, 0}; }},
-        {qc::Z,   [](const std::vector<fp>&) -> GateMatrix { return {1, 0, 0, -1}; }},
-        {qc::S,   [](const std::vector<fp>&) -> GateMatrix { return {1, 0, 0, {0, 1}}; }},
-        {qc::Sdg, [](const std::vector<fp>&) -> GateMatrix { return {1, 0, 0, {0, -1}}; }},
-        {qc::T,   [](const std::vector<fp>&) -> GateMatrix { return {1, 0, 0, {SQRT2_2, SQRT2_2}}; }},
-        {qc::Tdg, [](const std::vector<fp>&) -> GateMatrix { return {1, 0, 0, {SQRT2_2, -SQRT2_2}}; }},
-        {qc::SX,  [](const std::vector<fp>&) -> GateMatrix { return {std::complex{0.5, 0.5}, std::complex{0.5, -0.5}, std::complex{0.5, -0.5}, std::complex{0.5, 0.5}}; }},
-        {qc::SXdg,[](const std::vector<fp>&) -> GateMatrix { return {std::complex{0.5, -0.5}, std::complex{0.5, 0.5}, std::complex{0.5, 0.5}, std::complex{0.5, -0.5}}; }},
-        {qc::V,   [](const std::vector<fp>&) -> GateMatrix { return {SQRT2_2, {0., -SQRT2_2}, {0., -SQRT2_2}, SQRT2_2}; }},
-        {qc::Vdg, [](const std::vector<fp>&) -> GateMatrix { return {SQRT2_2, {0., SQRT2_2}, {0., SQRT2_2}, SQRT2_2}; }},
-        {qc::U,   [](const std::vector<fp>& params){ return uMat(params[0], params[1], params[2]); }},
-        {qc::U2,  [](const std::vector<fp>& params){ return u2Mat(params[0], params[1]); }},
-        {qc::P,   [](const std::vector<fp>& params){ return pMat(params[0]); }},
-        {qc::RX,  [](const std::vector<fp>& params){ return rxMat(params[0]); }},
-        {qc::RY,  [](const std::vector<fp>& params){ return ryMat(params[0]); }},
-        {qc::RZ,  [](const std::vector<fp>& params){ return rzMat(params[0]); }},
-        // clang-format on
-    }};
+GateMatrix opToSingleQubitGateMatrix(const qc::OpType t,
+                                     const std::vector<fp>& params) {
+  switch (t) {
+  case qc::I:
+    return {1, 0, 0, 1};
+  case qc::H:
+    return {SQRT2_2, SQRT2_2, SQRT2_2, -SQRT2_2};
+  case qc::X:
+    return {0, 1, 1, 0};
+  case qc::Y:
+    return {0, {0, -1}, {0, 1}, 0};
+  case qc::Z:
+    return {1, 0, 0, -1};
+  case qc::S:
+    return {1, 0, 0, {0, 1}};
+  case qc::Sdg:
+    return {1, 0, 0, {0, -1}};
+  case qc::T:
+    return {1, 0, 0, {SQRT2_2, SQRT2_2}};
+  case qc::Tdg:
+    return {1, 0, 0, {SQRT2_2, -SQRT2_2}};
+  case qc::SX:
+    return {std::complex{0.5, 0.5}, std::complex{0.5, -0.5},
+            std::complex{0.5, -0.5}, std::complex{0.5, 0.5}};
+  case qc::SXdg:
+    return {std::complex{0.5, -0.5}, std::complex{0.5, 0.5},
+            std::complex{0.5, 0.5}, std::complex{0.5, -0.5}};
+  case qc::V:
+    return {SQRT2_2, {0., -SQRT2_2}, {0., -SQRT2_2}, SQRT2_2};
+  case qc::Vdg:
+    return {SQRT2_2, {0., SQRT2_2}, {0., SQRT2_2}, SQRT2_2};
+  case qc::U:
+    // shuffle parameters to match semantics of parameter <-> matrix from
+    // getStandardOperationDD
+    return uMat(params.at(2), params.at(1), params.at(0));
+  case qc::U2:
+    // swap parameters to match semantics of parameter <-> matrix from
+    // getStandardOperationDD
+    return u2Mat(params.at(1), params.at(0));
+  case qc::P:
+    return pMat(params.at(0));
+  case qc::RX:
+    return rxMat(params.at(0));
+  case qc::RY:
+    return ryMat(params.at(0));
+  case qc::RZ:
+    return rzMat(params.at(0));
+  default:
+    throw qc::QFRException("Invalid single-qubit gate type");
+  }
+}
 
-const std::unordered_map<qc::OpType,
-                         TwoQubitGateMatrix (*const)(const std::vector<fp>&)>
-    TWO_GATE_MATS_GENERATORS{
-        // clang-format off
-        {qc::SWAP,      [](const std::vector<fp>&) -> TwoQubitGateMatrix {return {{ {1, 0, 0, 0}, {0, 0, 1, 0}, {0, 1, 0, 0}, {0, 0, 0, 1}}}; }},
-        {qc::iSWAP,     [](const std::vector<fp>&) -> TwoQubitGateMatrix {return {{ {1, 0, 0, 0}, {0, 0, {0, 1}, 0}, {0, {0, 1}, 0, 0}, {0, 0, 0, 1}}}; }},
-        {qc::iSWAPdg,   [](const std::vector<fp>&) -> TwoQubitGateMatrix {return {{ {1, 0, 0, 0}, {0, 0, {0, -1},0}, {0, {0, -1}, 0, 0}, {0, 0, 0, 1}}}; }},
-        {qc::ECR,       [](const std::vector<fp>&) -> TwoQubitGateMatrix {return {{ {0, 0, SQRT2_2, {0, SQRT2_2}}, {0, 0, {0, SQRT2_2}, SQRT2_2}, {SQRT2_2, {0, -SQRT2_2}, 0, 0}, {std::complex{0., -SQRT2_2}, SQRT2_2, 0, 0}}}; }},
-        {qc::DCX,       [](const std::vector<fp>&) -> TwoQubitGateMatrix {return {{{1, 0, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}, {0, 1, 0, 0}}}; }},
-        {qc::Peres,     [](const std::vector<fp>&) -> TwoQubitGateMatrix {return {{{0, 0, 0, 1}, {0, 0, 1, 0}, {1, 0, 0, 0}, {0, 1, 0, 0}}}; }},
-        {qc::Peresdg,   [](const std::vector<fp>&) -> TwoQubitGateMatrix {return {{ {0, 0, 1, 0}, {0, 0, 0, 1}, {0, 1, 0, 0}, {1, 0, 0, 0}}}; }},
-        {qc::RXX,       [](const std::vector<fp>& params){ return rxxMat(params[0]); }},
-        {qc::RYY,       [](const std::vector<fp>& params){ return ryyMat(params[0]); }},
-        {qc::RZZ,       [](const std::vector<fp>& params){ return rzzMat(params[0]); }},
-        {qc::RZX,       [](const std::vector<fp>& params){ return rzxMat(params[0]); }},
-        {qc::XXminusYY, [](const std::vector<fp>& params){ return xxMinusYYMat(params[0], params[1]); }},
-        {qc::XXplusYY,  [](const std::vector<fp>& params){ return xxPlusYYMat(params[0], params[1]); }},
-        // clang-format on
-    };
-
+TwoQubitGateMatrix opToTwoQubitGateMatrix(const qc::OpType t,
+                                          const std::vector<fp>& params) {
+  switch (t) {
+  case qc::SWAP:
+    return {{{1, 0, 0, 0}, {0, 0, 1, 0}, {0, 1, 0, 0}, {0, 0, 0, 1}}};
+  case qc::iSWAP:
+    return {{{1, 0, 0, 0}, {0, 0, {0, 1}, 0}, {0, {0, 1}, 0, 0}, {0, 0, 0, 1}}};
+  case qc::iSWAPdg:
+    return {
+        {{1, 0, 0, 0}, {0, 0, {0, -1}, 0}, {0, {0, -1}, 0, 0}, {0, 0, 0, 1}}};
+  case qc::ECR:
+    return {{{0, 0, SQRT2_2, {0, SQRT2_2}},
+             {0, 0, {0, SQRT2_2}, SQRT2_2},
+             {SQRT2_2, {0, -SQRT2_2}, 0, 0},
+             {std::complex{0., -SQRT2_2}, SQRT2_2, 0, 0}}};
+  case qc::DCX:
+    return {{{1, 0, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}, {0, 1, 0, 0}}};
+  case qc::Peres:
+    return {{{0, 0, 0, 1}, {0, 0, 1, 0}, {1, 0, 0, 0}, {0, 1, 0, 0}}};
+  case qc::Peresdg:
+    return {{{0, 0, 1, 0}, {0, 0, 0, 1}, {0, 1, 0, 0}, {1, 0, 0, 0}}};
+  case qc::RXX:
+    return rxxMat(params.at(0));
+  case qc::RYY:
+    return ryyMat(params.at(0));
+  case qc::RZZ:
+    return rzzMat(params.at(0));
+  case qc::RZX:
+    return rzxMat(params.at(0));
+  case qc::XXminusYY:
+    return xxMinusYYMat(params.at(0), params.at(1));
+  case qc::XXplusYY:
+    return xxPlusYYMat(params.at(0), params.at(1));
+  default:
+    throw qc::QFRException("Invalid two-qubit gate type");
+  }
+}
 } // namespace dd
