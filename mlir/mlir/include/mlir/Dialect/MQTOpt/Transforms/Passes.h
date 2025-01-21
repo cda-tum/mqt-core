c648901c409692eef1df451f0fead61636031492
Damian Rovara <damianrovara@gmail.com>
2025-01-21 13:33:44 +0000

    chore: :fire: Remove some files from old MQT transforms

--- mlir/mlir/include/mlir/Dialect/MQTOpt/Transforms/Passes.h
+++ mlir/mlir/include/mlir/Dialect/MQTOpt/Transforms/Passes.h
@@ -16,6 +16,7 @@ namespace mqt::ir::opt {
 #define GEN_PASS_DECL
 #include "mlir/Dialect/MQTOpt/Transforms/Passes.h.inc"
 
+void populateThePassPatterns(mlir::RewritePatternSet& patterns);
 void populateToQuantumComputationPatterns(mlir::RewritePatternSet& patterns,
                                           qc::QuantumComputation& circuit);
 void populateFromQuantumComputationPatterns(mlir::RewritePatternSet& patterns,
