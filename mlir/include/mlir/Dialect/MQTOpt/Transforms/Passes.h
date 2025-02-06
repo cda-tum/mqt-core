71c12cd5f97a335398919505cab93b2973196881
Damian Rovara <damianrovara@gmail.com>
2025-02-06 12:52:40 +0000

    feat(optimizations): :sparkles: add simple pass to cancel consecutive self-inverse gates

--- mlir/include/mlir/Dialect/MQTOpt/Transforms/Passes.h
+++ mlir/include/mlir/Dialect/MQTOpt/Transforms/Passes.h
@@ -15,7 +15,7 @@ namespace mqt::ir::opt {
 #define GEN_PASS_DECL
 #include "mlir/Dialect/MQTOpt/Transforms/Passes.h.inc"
 
-void populateThePassPatterns(mlir::RewritePatternSet& patterns);
+void populateCancelSelfInversePatterns(mlir::RewritePatternSet& patterns);
 void populateToQuantumComputationPatterns(mlir::RewritePatternSet& patterns,
                                           qc::QuantumComputation& circuit);
 void populateFromQuantumComputationPatterns(mlir::RewritePatternSet& patterns,
---
+++ mlir/lib/Dialect/MQTOpt/Transforms/CancelConsecutiveSelfInversePattern.cpp
@@ -0,0 +1,77 @@
+#include "mlir/Dialect/MQTOpt/IR/MQTOptDialect.h"
+#include "mlir/Dialect/MQTOpt/Transforms/Passes.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/ValueRange.h"
+#include "mlir/Support/LLVM.h"
+
+#include <mlir/Dialect/Arith/IR/Arith.h>
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/IR/PatternMatch.h>
+#include <mlir/Support/LogicalResult.h>
+#include <set>
+
+namespace mqt::ir::opt {
+
+struct CancelConsecutiveSelfInversePattern final
+    : mlir::OpInterfaceRewritePattern<UnitaryInterface> {
+
+  explicit CancelConsecutiveSelfInversePattern(mlir::MLIRContext* context)
+      : OpInterfaceRewritePattern(context) {}
+
+  mlir::LogicalResult match(UnitaryInterface op) const override {
+    if (!(mlir::isa<XOp>(op) || mlir::isa<YOp>(op) || mlir::isa<ZOp>(op) ||
+          mlir::isa<HOp>(op) || mlir::isa<IOp>(op) || mlir::isa<SWAPOp>(op))) {
+      return mlir::failure();
+    }
+    const auto& users = op->getUsers();
+    if (std::distance(users.begin(), users.end()) != 1) {
+      return mlir::failure();
+    }
+    auto user = *users.begin();
+    if (op->getName() != user->getName()) {
+      return mlir::failure();
+    }
+    auto unitaryUser = mlir::dyn_cast<UnitaryInterface>(user);
+    if (op.getOutQubits() != unitaryUser.getInQubits()) {
+      return mlir::failure();
+    }
+    return mlir::success();
+  }
+
+  void rewrite(UnitaryInterface op,
+               mlir::PatternRewriter& rewriter) const override {
+    auto user = mlir::dyn_cast<UnitaryInterface>(
+        *op->getUsers().begin()); // We always have exactly one user.
+    const auto& childUsers = user->getUsers();
+
+    for (const auto& childUser : childUsers) {
+      for (size_t i = 0; i < childUser->getOperands().size(); i++) {
+        const auto& operand = childUser->getOperand(i);
+        const auto found = std::find(user.getOutQubits().begin(),
+                                     user.getOutQubits().end(), operand);
+        if (found == user.getOutQubits().end()) {
+          continue;
+        }
+        const auto idx = std::distance(user.getOutQubits().begin(), found);
+        rewriter.modifyOpInPlace(childUser, [&] {
+          childUser->setOperand(i, op.getInQubits()[idx]);
+        });
+      }
+    }
+
+    rewriter.eraseOp(user);
+    rewriter.eraseOp(op);
+  }
+};
+
+/**
+ * @brief Populates the given pattern set with the
+ * `CancelConsecutiveSelfInversePattern`.
+ *
+ * @param patterns The pattern set to populate.
+ */
+void populateCancelSelfInversePatterns(mlir::RewritePatternSet& patterns) {
+  patterns.add<CancelConsecutiveSelfInversePattern>(patterns.getContext());
+}
+
+} // namespace mqt::ir::opt
