16e022f17530c2581c5ded98b600920a171da7a0
M-J-Hochreiter <77507116+M-J-Hochreiter@users.noreply.github.com>
2025-02-21 23:25:33 +0100

    ✨ Add State Preparation Algorithm (#543)
    ## Description
    
    As a part of my bachelor thesis I implemented the Qiskit State
    Preparation algorithm
    (https://github.com/Qiskit/qiskit/blob/main/qiskit/circuit/library/data_preparation/state_preparation.py#)
    in C++ using the MQT Framework.
    My advisor Yannick Stade suggested I open a pull request, as it might be
    interesting to add into MQT.
    
    The state preparation allows a user to create a circuit that initializes
    a quantum state from a list of complex amplitudes, sometime needed in
    various quantum algorithms.
    
    I am not sure how/where to add this class, thus I just created a new
    file.
    If you could give me feedback whether this functionality might be
    interesting to add/what to change I would gladly try my best to fulfill
    your requests.
    
    I have not yet written any tests, apart from manually comparing outputs
    of qiskit and my algorithm, and simulating both circuits with qiskit.
    (can be added in the future)
    
    ## Checklist:
    
    <!---
    This checklist serves as a reminder of a couple of things that ensure
    your pull request will be merged swiftly.
    -->
    
    - [x] The pull request only contains commits that are related to it.
    - [x] I have added appropriate tests and documentation.
    - [x] I have made sure that all CI jobs on GitHub pass.
    - [x] The pull request introduces no new warnings and follows the
    project's style guidelines.
    
    ---------
    
    Signed-off-by: M-J-Hochreiter <77507116+M-J-Hochreiter@users.noreply.github.com>
    Signed-off-by: burgholzer <burgholzer@me.com>
    Signed-off-by: Lukas Burgholzer <burgholzer@me.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Lukas Burgholzer <burgholzer@me.com>

---
+++ test/algorithms/test_statepreparation.cpp
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2025 Chair for Design Automation, TUM
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License
+ */
+
+#include "algorithms/StatePreparation.hpp"
+#include "dd/Package.hpp"
+#include "dd/Simulation.hpp"
+#include "ir/QuantumComputation.hpp"
+
+#include <cmath>
+#include <complex>
+#include <cstddef>
+#include <gtest/gtest.h>
+#include <memory>
+#include <stdexcept>
+#include <tuple>
+#include <vector>
+
+constexpr double EPS = 1e-10;
+
+class StatePreparation
+    : public testing::TestWithParam<std::vector<std::complex<double>>> {
+protected:
+  std::vector<std::complex<double>> amplitudes;
+
+  void TearDown() override {}
+  void SetUp() override { amplitudes = GetParam(); }
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    StatePreparation, StatePreparation,
+    testing::Values(
+        std::vector<std::complex<double>>{std::complex{1 / std::sqrt(2)},
+                                          std::complex{-1 / std::sqrt(2)}},
+        std::vector<std::complex<double>>{
+            std::complex<double>{1 / std::sqrt(2)},
+            std::complex<double>{0, -1 / std::sqrt(2)}},
+        std::vector<std::complex<double>>{0, std::complex{1 / std::sqrt(2)},
+                                          std::complex{-1 / std::sqrt(2)}, 0},
+        std::vector<std::complex<double>>{
+            std::complex<double>{1 / std::sqrt(13)},
+            std::complex<double>{-1 / std::sqrt(13)},
+            std::complex<double>{1 / std::sqrt(13), -1 / std::sqrt(13)},
+            std::complex<double>{0, 3 / std::sqrt(13)}},
+        std::vector<std::complex<double>>{
+            std::complex<double>{1. / 4}, std::complex<double>{1. / 4},
+            std::complex<double>{1. / 4}, std::complex<double>{1. / 4},
+            std::complex<double>{1. / 4}, std::complex<double>{1. / 4},
+            std::complex<double>{1. / 4}, std::complex<double>{3. / 4}},
+        std::vector<std::complex<double>>{
+            std::complex<double>{1. / 4}, std::complex<double>{0, 1. / 4},
+            std::complex<double>{1. / 4}, std::complex<double>{0, 1. / 4},
+            std::complex<double>{0, 1. / 4}, std::complex<double>{1. / 4},
+            std::complex<double>{1. / 4}, std::complex<double>{3. / 4}}));
+
+TEST_P(StatePreparation, StatePreparationCircuitSimulation) {
+  const auto expectedAmplitudes = GetParam();
+  qc::QuantumComputation qc;
+  ASSERT_NO_THROW({ qc = qc::createStatePreparationCircuit(amplitudes); });
+  auto dd = std::make_unique<dd::Package<>>(qc.getNqubits());
+  qc::VectorDD e{};
+  ASSERT_NO_THROW(
+      { e = dd::simulate(qc, dd->makeZeroState(qc.getNqubits()), *dd); });
+  auto result = e.getVector();
+  for (size_t i = 0; i < expectedAmplitudes.size(); ++i) {
+    ASSERT_NEAR(expectedAmplitudes[i].real(), result[i].real(), EPS);
+    ASSERT_NEAR(expectedAmplitudes[i].imag(), result[i].imag(), EPS);
+  }
+}
+
+TEST(StatePreparation, StatePreparationAmplitudesNotNormalized) {
+  const auto amplitudes = std::vector<std::complex<double>>{
+      std::complex<double>{1}, std::complex<double>{1}};
+
+  ASSERT_THROW(std::ignore = qc::createStatePreparationCircuit(amplitudes),
+               std::invalid_argument);
+}
+
+TEST(StatePreparation, StatePreparationsAmplitudesNotPowerOf2) {
+  const auto amplitudes =
+      std::vector<std::complex<double>>{std::complex<double>{1 / std::sqrt(3)},
+                                        std::complex<double>{1 / std::sqrt(3)},
+                                        std::complex<double>{1 / std::sqrt(3)}};
+
+  ASSERT_THROW(std::ignore = qc::createStatePreparationCircuit(amplitudes),
+               std::invalid_argument);
+}
