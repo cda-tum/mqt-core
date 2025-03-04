2f88fd7b17fb3d06c31faa107d27be431b0fdef4
Florea Ioan-Albert <ge54dox@mytum.de>
2025-03-04 15:37:19 +0100

    :fire: Removed docker functionality used in the begging of the MLIR project

--- mlir/docker/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2025 Chair for Design Automation, TUM
-# All rights reserved.
-#
-# SPDX-License-Identifier: MIT
-#
-# Licensed under the MIT License
-
-docker run --rm -it -v $(pwd)/..:/home/mqt/mqt-mlir mqt-catalyst
