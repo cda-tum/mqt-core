6a5c1541fc4d120a58cc23025fd170e1a0ac0eb7
burgholzer <burgholzer@me.com>
2025-03-16 21:44:50 +0100

    🎨 clean up `Node.hpp`
    mostly adding some `[[nodiscard]]` and adjusting documentation, plus adding some `final` modifiers to the leaf classes
    
    Signed-off-by: burgholzer <burgholzer@me.com>

--- include/mqt-core/dd/Node.hpp
+++ include/mqt-core/dd/Node.hpp
@@ -22,31 +22,45 @@
 namespace dd {
 
 /**
- * @brief Base class for all DD nodes
- * @details This class is used to store common information for all DD nodes. The
- * `flags` field is an exception, but it allows to reuse functionality for
- * different node types. Data Layout |8|4|2|1| = 15B (space for one more byte)
+ * @brief Base class for all DD nodes.
+ * @details This class is used to store common information for all DD nodes.
+ * The `flags` makes the implicit padding explicit and can be used for storing
+ * node properties.
+ * Data Layout (8)|(4|2|2) = 16B.
  */
-struct NodeBase : public LLBase {
-  RefCount ref = 0; // reference count
-  Qubit v{};        // variable index
+struct NodeBase : LLBase {
+  /// Reference count
+  RefCount ref = 0;
+  /// Variable index
+  Qubit v{};
 
-  std::uint8_t flags =
-      0; // not required for all node types, but padding is required either way
-  // 32 = unused (was used to mark a node which is symmetric)
-  // 16 = unused (was used to mark a node resembling the identity)
-  // 8 = marks a reduced dm node,
-  // 4 = marks a dm (tmp flag),
-  // 2 = mark first path edge (tmp flag),
-  // 1 = mark path is conjugated (tmp flag)
+  /**
+   * @brief Flags for node properties
+   * @details Not required for all node types, but padding is required either
+   * way.
+   *
+   * 0b1000 = marks a reduced dm node,
+   *  0b100 = marks a dm (tmp flag),
+   *   0b10 = mark first path edge (tmp flag),
+   *    0b1 = mark path is conjugated (tmp flag)
+   */
+  std::uint16_t flags = 0;
 
-  NodeBase* next() const noexcept {
-    return static_cast<NodeBase*>(LLBase::next());
+  /// Getter for the next object.
+  [[nodiscard]] NodeBase* next() const noexcept {
+    return static_cast<NodeBase*>(next_);
   }
 
-  static bool isTerminal(const NodeBase* p) noexcept {
-    // some nodes (dNode) use bottom bits to store flags. As all nodes are >= 8
-    // byte aligned, so these bits can be ignored in all cases
+  /**
+   * @brief Check if a node is terminal
+   * @details Generally, a node is terminal if it is nullptr.
+   * Some nodes (dNode) encode additional information in the least significant
+   * bits of the pointer. These bits are masked out before checking for
+   * terminal nodes.
+   * @param p The node to check
+   * @return true if the node is terminal, false otherwise.
+   */
+  [[nodiscard]] static bool isTerminal(const NodeBase* p) noexcept {
     return (reinterpret_cast<std::uintptr_t>(p) & (~7ULL)) == 0ULL;
   }
   static constexpr NodeBase* getTerminal() noexcept { return nullptr; }
@@ -54,13 +68,16 @@ struct NodeBase : public LLBase {
 
 /**
  * @brief A vector DD node
- * @details Data Layout 8|4|2|1|1 (padding)|24|24 = 64B
+ * @details Data Layout (8)|(4|2|2)|(24|24) = 64B
  */
-struct vNode : public NodeBase {      // NOLINT(readability-identifier-naming)
+struct vNode final : NodeBase {       // NOLINT(readability-identifier-naming)
   std::array<Edge<vNode>, RADIX> e{}; // edges out of this node
 
-  vNode* next() const noexcept { return static_cast<vNode*>(NodeBase::next()); }
-
+  /// Getter for the next object
+  [[nodiscard]] vNode* next() const noexcept {
+    return static_cast<vNode*>(next_);
+  }
+  /// Getter for the terminal object
   static constexpr vNode* getTerminal() noexcept { return nullptr; }
 };
 using vEdge = Edge<vNode>;
@@ -68,13 +85,16 @@ using vCachedEdge = CachedEdge<vNode>;
 
 /**
  * @brief A matrix DD node
- * @details Data Layout 8|4|2|1|1 (padding)|24|24|24|24|= 112B
+ * @details Data Layout (8)|(4|2|2)|(24|24|24|24) = 112B
  */
-struct mNode : public NodeBase {      // NOLINT(readability-identifier-naming)
+struct mNode final : NodeBase {       // NOLINT(readability-identifier-naming)
   std::array<Edge<mNode>, NEDGE> e{}; // edges out of this node
 
-  mNode* next() const noexcept { return static_cast<mNode*>(NodeBase::next()); }
-
+  /// Getter for the next object
+  [[nodiscard]] mNode* next() const noexcept {
+    return static_cast<mNode*>(next_);
+  }
+  /// Getter for the terminal object
   static constexpr mNode* getTerminal() noexcept { return nullptr; }
 };
 using mEdge = Edge<mNode>;
@@ -82,13 +102,16 @@ using mCachedEdge = CachedEdge<mNode>;
 
 /**
  * @brief A density matrix DD node
- * @details Data Layout 8|4|2|1|1 (padding)|24|24|24|24|8|4|2|1| = 112B
+ * @details Data Layout (8)|(4|2|2)|(24|24|24|24) = 112B
  */
-struct dNode : public NodeBase {      // NOLINT(readability-identifier-naming)
+struct dNode final : NodeBase {       // NOLINT(readability-identifier-naming)
   std::array<Edge<dNode>, NEDGE> e{}; // edges out of this node
 
-  dNode* next() const noexcept { return static_cast<dNode*>(NodeBase::next()); }
-
+  /// Getter for the next object
+  [[nodiscard]] dNode* next() const noexcept {
+    return static_cast<dNode*>(next_);
+  }
+  /// Getter for the terminal object
   static constexpr dNode* getTerminal() noexcept { return nullptr; }
 
   [[nodiscard]] [[maybe_unused]] static constexpr bool
@@ -184,7 +207,7 @@ static inline dEdge densityFromMatrixEdge(const mEdge& e) {
  * @note Typically, you do not want to call this function directly. Instead,
  * use the UniqueTable::incRef(Node*) function.
  */
-[[nodiscard]] static inline bool incRef(NodeBase* p) noexcept {
+[[nodiscard]] static constexpr bool incRef(NodeBase* p) noexcept {
   if (p == nullptr || p->ref == std::numeric_limits<RefCount>::max()) {
     return false;
   }
@@ -202,7 +225,7 @@ static inline dEdge densityFromMatrixEdge(const mEdge& e) {
  * @note Typically, you do not want to call this function directly. Instead,
  * use the UniqueTable::decRef(Node*) function.
  */
-[[nodiscard]] static inline bool decRef(NodeBase* p) noexcept {
+[[nodiscard]] static constexpr bool decRef(NodeBase* p) noexcept {
   if (p == nullptr || p->ref == std::numeric_limits<RefCount>::max()) {
     return false;
   }
