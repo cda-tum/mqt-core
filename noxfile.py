54c4b93a99fc761ee0edacb5dffa39a947f3fc4c
burgholzer <burgholzer@me.com>
2025-03-16 12:44:24 +0100

    Revert "⚗️ only run nox on a single Python version"
    This reverts commit 9ca1989f6ec455e683d4eeeb8a2e3feaf34978b2.

--- noxfile.py
+++ noxfile.py
@@ -26,7 +26,7 @@ nox.options.default_venv_backend = "uv"
 
 nox.options.sessions = ["lint", "tests", "minimums"]
 
-PYTHON_ALL_VERSIONS = ["3.9"]
+PYTHON_ALL_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13"]
 
 if os.environ.get("CI", None):
     nox.options.error_on_missing_interpreters = True
