584b5d9e27f8724e3e66e1b34dced445b6844702
burgholzer <burgholzer@me.com>
2025-03-16 12:44:23 +0100

    Revert "⚗️ include run that worked most of the time"
    This reverts commit d4b9519d683c5c6b949a21b52bd1be8230dcd802.

--- noxfile.py
+++ noxfile.py
@@ -26,7 +26,7 @@ nox.options.default_venv_backend = "uv"
 
 nox.options.sessions = ["lint", "tests", "minimums"]
 
-PYTHON_ALL_VERSIONS = ["3.9", "3.12"]
+PYTHON_ALL_VERSIONS = ["3.9"]
 
 if os.environ.get("CI", None):
     nox.options.error_on_missing_interpreters = True
