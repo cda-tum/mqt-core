41fdd786f651dad87b8aafdab9f03ce7e1453f59
burgholzer <burgholzer@me.com>
2025-03-16 21:42:52 +0100

    🎨 clean up `LinkedListBase.hpp`
    Signed-off-by: burgholzer <burgholzer@me.com>

--- include/mqt-core/dd/LinkedListBase.hpp
+++ include/mqt-core/dd/LinkedListBase.hpp
@@ -7,38 +7,37 @@
  * Licensed under the MIT License
  */
 
+/** @file
+ * @brief Linked list functionality required for UniqueTable and MemoryManager.
+ */
+
 #pragma once
 
-#include "dd/DDDefinitions.hpp"
-
-#include <array>
-#include <cstddef>
 namespace dd {
 
 /**
- * @brief Members required UniqueTable and MemoryManager
+ * @brief A class to provide a base for linked list objects
  */
 struct LLBase {
   /**
-   * @brief The pointer tho the next object
-   * @details The next pointer is used to from linked lists. Classes that should
-   * be used in a linked list must solely inherit from this class. Other code in
-   * mqt-core relies on this assumption that all objects in a linked list are of
-   * the exact same type.
+   * @brief The pointer to the next object
+   * @details The next pointer is used to form linked lists of objects.
+   * Classes used in a linked list must solely inherit from this class.
+   * Other code in mqt-core relies on the assumption that all objects in a
+   * linked list are of the same type.
    */
-
-  LLBase* next_ = nullptr; // used to link nodes in unique table
+  LLBase* next_ = nullptr;
 
   /**
-   * @brief default method to get the next object
+   * @brief Default getter for the next object
    * @details Classes that inherit from LLBase should implement their own next()
    * method to return the next object in the list with a specialized return
    * type.
    * @return LLBase*
    */
-  LLBase* next() const noexcept { return next_; }
+  [[nodiscard]] LLBase* next() const noexcept { return next_; }
 
-  // set the pointer to the next object
+  /// Setter for the next object
   void setNext(LLBase* n) noexcept { next_ = n; }
 };
 
