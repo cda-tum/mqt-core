06d15dd54b5ad482b9326572db0afdbc1d9fe8fd
burgholzer <burgholzer@me.com>
2025-03-24 11:20:58 +0100

    🚚 move DD operations code to source file
    Signed-off-by: burgholzer <burgholzer@me.com>

--- include/mqt-core/dd/Operations.hpp
+++ include/mqt-core/dd/Operations.hpp
@@ -16,7 +16,6 @@
 #include "ir/Definitions.hpp"
 #include "ir/Permutation.hpp"
 #include "ir/operations/ClassicControlledOperation.hpp"
-#include "ir/operations/CompoundOperation.hpp"
 #include "ir/operations/Control.hpp"
 #include "ir/operations/NonUnitaryOperation.hpp"
 #include "ir/operations/OpType.hpp"
@@ -25,11 +24,8 @@
 
 #include <algorithm>
 #include <cassert>
-#include <cmath>
 #include <cstddef>
-#include <ostream>
 #include <random>
-#include <sstream>
 #include <stdexcept>
 #include <string>
 #include <utility>
@@ -51,28 +47,10 @@ namespace dd {
  * @param targets The operation targets
  * @return The decision diagram representation of the operation
  */
-inline MatrixDD getStandardOperationDD(Package& dd, const qc::OpType type,
-                                       const std::vector<fp>& params,
-                                       const qc::Controls& controls,
-                                       const std::vector<qc::Qubit>& targets) {
-  if (qc::isSingleQubitGate(type)) {
-    if (targets.size() != 1) {
-      throw std::invalid_argument(
-          "Expected exactly one target qubit for single-qubit gate");
-    }
-    return dd.makeGateDD(opToSingleQubitGateMatrix(type, params), controls,
-                         targets[0U]);
-  }
-  if (qc::isTwoQubitGate(type)) {
-    if (targets.size() != 2) {
-      throw std::invalid_argument(
-          "Expected two target qubits for two-qubit gate");
-    }
-    return dd.makeTwoQubitGateDD(opToTwoQubitGateMatrix(type, params), controls,
-                                 targets[0U], targets[1U]);
-  }
-  throw std::runtime_error("Unexpected operation type");
-}
+MatrixDD getStandardOperationDD(Package& dd, qc::OpType type,
+                                const std::vector<fp>& params,
+                                const qc::Controls& controls,
+                                const std::vector<qc::Qubit>& targets);
 
 /**
  * @brief Get the decision diagram representation of a @ref
@@ -88,92 +66,10 @@ inline MatrixDD getStandardOperationDD(Package& dd, const qc::OpType type,
  * @param inverse Whether to get the inverse of the operation
  * @return The decision diagram representation of the operation
  */
-inline MatrixDD getStandardOperationDD(const qc::StandardOperation& op,
-                                       Package& dd,
-                                       const qc::Controls& controls,
-                                       const std::vector<qc::Qubit>& targets,
-                                       const bool inverse) {
-  auto type = op.getType();
-
-  if (!inverse) {
-    return getStandardOperationDD(dd, type, op.getParameter(), controls,
-                                  targets);
-  }
-
-  // invert the operation
-  std::vector<fp> params = op.getParameter();
-  std::vector<qc::Qubit> targetQubits = targets;
-
-  switch (type) {
-  // operations that are self-inverse do not need any changes
-  case qc::I:
-  case qc::H:
-  case qc::X:
-  case qc::Y:
-  case qc::Z:
-  case qc::SWAP:
-  case qc::ECR:
-    break;
-  // operations that have an inverse gate with the same parameters
-  case qc::iSWAP:
-  case qc::iSWAPdg:
-  case qc::Peres:
-  case qc::Peresdg:
-  case qc::S:
-  case qc::Sdg:
-  case qc::T:
-  case qc::Tdg:
-  case qc::V:
-  case qc::Vdg:
-  case qc::SX:
-  case qc::SXdg:
-    type = static_cast<qc::OpType>(+type ^ qc::OpTypeInv);
-    break;
-  // operations that can be inversed by negating the first parameter
-  case qc::RXX:
-  case qc::RYY:
-  case qc::RZZ:
-  case qc::RZX:
-  case qc::RX:
-  case qc::RY:
-  case qc::RZ:
-  case qc::P:
-  case qc::XXminusYY:
-  case qc::XXplusYY:
-    params[0U] = -params[0U];
-    break;
-  // other special cases
-  case qc::DCX:
-    if (targetQubits.size() != 2) {
-      throw std::runtime_error("Invalid target qubits for DCX");
-    }
-    // DCX is not self-inverse, but the inverse is just swapping the targets
-    std::swap(targetQubits[0], targetQubits[1]);
-    break;
-  // invert all parameters
-  case qc::U:
-    // swap [a, b, c] to [a, c, b]
-    std::swap(params[1U], params[2U]);
-    for (auto& param : params) {
-      param = -param;
-    }
-    break;
-  case qc::U2:
-    std::swap(params[0U], params[1U]);
-    params[0U] = -params[0U] - PI;
-    params[1U] = -params[1U] + PI;
-    break;
-
-  default:
-    std::ostringstream oss{};
-    oss << "negation for gate " << op.getName() << " not available!";
-    throw std::runtime_error(oss.str());
-  }
-  return getStandardOperationDD(dd, type, params, controls, targetQubits);
-}
-
-MatrixDD getInverseDD(const qc::Operation& op, Package& dd,
-                      const qc::Permutation& permutation = {});
+MatrixDD getStandardOperationDD(const qc::StandardOperation& op, Package& dd,
+                                const qc::Controls& controls,
+                                const std::vector<qc::Qubit>& targets,
+                                bool inverse);
 
 /**
  * @brief Get the decision diagram representation of an operation.
@@ -185,57 +81,8 @@ MatrixDD getInverseDD(const qc::Operation& op, Package& dd,
  * @param inverse Whether to get the inverse of the operation
  * @return The decision diagram representation of the operation
  */
-inline MatrixDD getDD(const qc::Operation& op, Package& dd,
-                      const qc::Permutation& permutation = {},
-                      const bool inverse = false) {
-  const auto type = op.getType();
-
-  if (type == qc::Barrier) {
-    return dd.makeIdent();
-  }
-
-  if (type == qc::GPhase) {
-    auto phase = op.getParameter()[0U];
-    if (inverse) {
-      phase = -phase;
-    }
-    auto id = dd.makeIdent();
-    id.w = dd.cn.lookup(std::cos(phase), std::sin(phase));
-    return id;
-  }
-
-  if (op.isStandardOperation()) {
-    const auto& standardOp = dynamic_cast<const qc::StandardOperation&>(op);
-    const auto& targets = permutation.apply(standardOp.getTargets());
-    const auto& controls = permutation.apply(standardOp.getControls());
-
-    return getStandardOperationDD(standardOp, dd, controls, targets, inverse);
-  }
-
-  if (op.isCompoundOperation()) {
-    const auto& compoundOp = dynamic_cast<const qc::CompoundOperation&>(op);
-    auto e = dd.makeIdent();
-    if (inverse) {
-      for (const auto& operation : compoundOp) {
-        e = dd.multiply(e, getInverseDD(*operation, dd, permutation));
-      }
-    } else {
-      for (const auto& operation : compoundOp) {
-        e = dd.multiply(getDD(*operation, dd, permutation), e);
-      }
-    }
-    return e;
-  }
-
-  if (op.isClassicControlledOperation()) {
-    const auto& classicOp =
-        dynamic_cast<const qc::ClassicControlledOperation&>(op);
-    return getDD(*classicOp.getOperation(), dd, permutation, inverse);
-  }
-
-  assert(op.isNonUnitaryOperation());
-  throw std::invalid_argument("DD for non-unitary operation not available!");
-}
+MatrixDD getDD(const qc::Operation& op, Package& dd,
+               const qc::Permutation& permutation = {}, bool inverse = false);
 
 /**
  * @brief Get the decision diagram representation of the inverse of an
@@ -249,19 +96,16 @@ inline MatrixDD getDD(const qc::Operation& op, Package& dd,
  * empty permutation marks the identity permutation.
  * @return The decision diagram representation of the inverse of the operation
  */
-inline MatrixDD getInverseDD(const qc::Operation& op, Package& dd,
-                             const qc::Permutation& permutation) {
-  return getDD(op, dd, permutation, true);
-}
+MatrixDD getInverseDD(const qc::Operation& op, Package& dd,
+                      const qc::Permutation& permutation = {});
 
 /**
- * @brief Apply a unitary operation to a given DD.
+ * @brief Apply a unitary operation to a given vector DD.
  *
  * @details This is a convenience function that realizes @p op times @p in and
  * correctly accounts for the permutation of the operation's qubits as well as
  * automatically handles reference counting.
  *
- * @tparam Node The type of the DD nodes
  * @param op The operation to apply
  * @param in The input DD
  * @param dd The DD package to use
@@ -269,13 +113,30 @@ inline MatrixDD getInverseDD(const qc::Operation& op, Package& dd,
  * empty permutation marks the identity permutation.
  * @return The output DD
  */
-template <class Node>
-Edge<Node> applyUnitaryOperation(const qc::Operation& op, const Edge<Node>& in,
-                                 Package& dd,
-                                 const qc::Permutation& permutation = {}) {
-  static_assert(std::is_same_v<Node, vNode> || std::is_same_v<Node, mNode>);
-  return dd.applyOperation(getDD(op, dd, permutation), in);
-}
+VectorDD applyUnitaryOperation(const qc::Operation& op, const VectorDD& in,
+                               Package& dd,
+                               const qc::Permutation& permutation = {});
+
+/**
+ * @brief Apply a unitary operation to a given matrix DD.
+ *
+ * @details This is a convenience function that realizes @p op times @p in and
+ * correctly accounts for the permutation of the operation's qubits as well as
+ * automatically handles reference counting.
+ *
+ * @param op The operation to apply
+ * @param in The input DD
+ * @param dd The DD package to use
+ * @param permutation The permutation to apply to the operation's qubits. An
+ * empty permutation marks the identity permutation.
+ * @param applyFromLeft Whether to apply the operation from the left (true)
+ * or from the right (false).
+ * @return The output DD
+ */
+MatrixDD applyUnitaryOperation(const qc::Operation& op, const MatrixDD& in,
+                               Package& dd,
+                               const qc::Permutation& permutation = {},
+                               bool applyFromLeft = true);
 
 /**
  * @brief Apply a measurement operation to a given DD.
@@ -295,20 +156,10 @@ Edge<Node> applyUnitaryOperation(const qc::Operation& op, const Edge<Node>& in,
  * empty permutation marks the identity permutation.
  * @return The output DD
  */
-inline VectorDD applyMeasurement(const qc::NonUnitaryOperation& op, VectorDD in,
-                                 Package& dd, std::mt19937_64& rng,
-                                 std::vector<bool>& measurements,
-                                 const qc::Permutation& permutation = {}) {
-  assert(op.getType() == qc::Measure);
-  const auto& qubits = permutation.apply(op.getTargets());
-  const auto& bits = op.getClassics();
-  for (size_t j = 0U; j < qubits.size(); ++j) {
-    measurements.at(bits.at(j)) =
-        dd.measureOneCollapsing(in, static_cast<dd::Qubit>(qubits.at(j)),
-                                rng) == '1';
-  }
-  return in;
-}
+VectorDD applyMeasurement(const qc::NonUnitaryOperation& op, VectorDD in,
+                          Package& dd, std::mt19937_64& rng,
+                          std::vector<bool>& measurements,
+                          const qc::Permutation& permutation = {});
 
 /**
  * @brief Apply a reset operation to a given DD.
@@ -327,22 +178,9 @@ inline VectorDD applyMeasurement(const qc::NonUnitaryOperation& op, VectorDD in,
  * empty permutation marks the identity permutation.
  * @return The output DD
  */
-inline VectorDD applyReset(const qc::NonUnitaryOperation& op, VectorDD in,
-                           Package& dd, std::mt19937_64& rng,
-                           const qc::Permutation& permutation = {}) {
-  assert(op.getType() == qc::Reset);
-  const auto& qubits = permutation.apply(op.getTargets());
-  for (const auto& qubit : qubits) {
-    const auto bit =
-        dd.measureOneCollapsing(in, static_cast<dd::Qubit>(qubit), rng);
-    // apply an X operation whenever the measured result is one
-    if (bit == '1') {
-      const auto x = qc::StandardOperation(qubit, qc::X);
-      in = applyUnitaryOperation(x, in, dd);
-    }
-  }
-  return in;
-}
+VectorDD applyReset(const qc::NonUnitaryOperation& op, VectorDD in, Package& dd,
+                    std::mt19937_64& rng,
+                    const qc::Permutation& permutation = {});
 
 /**
  * @brief Apply a classic controlled operation to a given DD.
@@ -362,58 +200,11 @@ inline VectorDD applyReset(const qc::NonUnitaryOperation& op, VectorDD in,
  * empty permutation marks the identity permutation.
  * @return The output DD
  */
-inline VectorDD
+VectorDD
 applyClassicControlledOperation(const qc::ClassicControlledOperation& op,
                                 const VectorDD& in, Package& dd,
                                 const std::vector<bool>& measurements,
-                                const qc::Permutation& permutation = {}) {
-  const auto& expectedValue = op.getExpectedValue();
-  const auto& comparisonKind = op.getComparisonKind();
-
-  // determine the actual value from measurements
-  auto actualValue = 0ULL;
-  if (const auto& controlRegister = op.getControlRegister();
-      controlRegister.has_value()) {
-    assert(!op.getControlBit().has_value());
-    const auto regStart = controlRegister->getStartIndex();
-    const auto regSize = controlRegister->getSize();
-    for (std::size_t j = 0; j < regSize; ++j) {
-      if (measurements[regStart + j]) {
-        actualValue |= 1ULL << j;
-      }
-    }
-  }
-  if (const auto& controlBit = op.getControlBit(); controlBit.has_value()) {
-    assert(!op.getControlRegister().has_value());
-    actualValue = measurements[*controlBit] ? 1U : 0U;
-  }
-
-  // check if the actual value matches the expected value according to the
-  // comparison kind
-  const auto control = [actualValue, expectedValue, comparisonKind]() {
-    switch (comparisonKind) {
-    case qc::ComparisonKind::Eq:
-      return actualValue == expectedValue;
-    case qc::ComparisonKind::Neq:
-      return actualValue != expectedValue;
-    case qc::ComparisonKind::Lt:
-      return actualValue < expectedValue;
-    case qc::ComparisonKind::Leq:
-      return actualValue <= expectedValue;
-    case qc::ComparisonKind::Gt:
-      return actualValue > expectedValue;
-    case qc::ComparisonKind::Geq:
-      return actualValue >= expectedValue;
-    }
-    qc::unreachable();
-  }();
-
-  if (!control) {
-    return in;
-  }
-
-  return applyUnitaryOperation(op, in, dd, permutation);
-}
+                                const qc::Permutation& permutation = {});
 
 /**
  * @brief Change the permutation of a given DD.
